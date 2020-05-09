11ca9449855b8e6ff1e5a53287870b853e98ec80
skittles1412 <61439840+skittles1412@users.noreply.github.com>
2020-05-09 07:48:00 -0700

    Add solution for problem `A - Sum of Round Numbers` Link: https://codeforces.com/contest/1352/problem/A Tags: implementation, math, *800 Ref: https://codeforces.com/contest/1352/submission/79485416

---
+++ codeforces/1352/A.java
@@ -0,0 +1,92 @@
+//package round_640;
+
+import java.util.*;
+import java.io.*;
+
+public class Problem_A {
+	static class Input {
+		BufferedReader br;
+		StringTokenizer st;
+		public Input(Reader r) throws IOException {
+			br = new BufferedReader(r);
+			st = new StringTokenizer(br.readLine());
+		}
+		public String nextLine() throws IOException {
+			return br.readLine();
+		}
+		public boolean hasNext() throws IOException {
+			while(!st.hasMoreTokens()) {
+				String s = br.readLine();
+				if(s==null) {
+					return false;
+				}
+				st = new StringTokenizer(s);
+			}
+			return true;
+		}
+		public String next() throws IOException {
+			if(!hasNext()) {
+				return null;
+			}
+			return st.nextToken();
+		}
+		public int nextInt() throws IOException {
+			String s = next();
+			if(s==null) {
+				return Integer.MIN_VALUE;
+			}
+			return Integer.parseInt(s);
+		}
+		public double nextDouble() throws IOException {
+			String s = next();
+			if(s==null) {
+				return Double.MIN_VALUE;
+			}
+			return Double.parseDouble(s);
+		}
+		public long nextLong() throws IOException {
+			String s = next();
+			if(s==null) {
+				return Long.MIN_VALUE;
+			}
+			return Long.parseLong(s);
+		}
+		public int countTokens() {
+			return st.countTokens();
+		}
+		public void close() throws IOException {
+			br.close();
+		}
+	}
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		Problem_A program = new Problem_A();
+		program.Begin();
+	}
+	void Begin() throws IOException{
+		Input in = new Input(new InputStreamReader(System.in));
+		PrintWriter pw = new PrintWriter(new BufferedOutputStream(System.out));
+		int kase = in.nextInt();
+		while(kase-->0) {
+			int n = in.nextInt();
+			ArrayList<Integer> al = new ArrayList<Integer>();
+			for(int i = 10; n!=0; i*=10) {
+				int cur = n%i;
+				if(cur!=0) {
+					al.add(cur);
+				}
+				n-=cur;
+			}
+			pw.println(al.size());
+			for(int i = 0; i<al.size(); i++) {
+				if(i!=0) {
+					pw.print(" ");
+				}
+				pw.print(al.get(i));
+			}
+			pw.println();
+		}
+		pw.close();
+		in.close();
+	}
+}
