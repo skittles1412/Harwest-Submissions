58534c7abdb4d50a2e0abfcd47621769e8e80bcb
skittles1412 <61439840+skittles1412@users.noreply.github.com>
2020-05-26 07:51:00 -0700

    Add solution for problem `B - Maria Breaks the Self-isolation` Link: https://codeforces.com/contest/1358/problem/B Tags: greedy, sortings, *1000 Ref: https://codeforces.com/contest/1358/submission/81495914

---
+++ codeforces/1358/B.java
@@ -0,0 +1,94 @@
+
+import java.util.*;
+import java.io.*;
+
+public class Problem_B {
+	static class Input {
+		BufferedReader br;
+		StringTokenizer st;
+		public Input(Reader r) throws IOException {
+			br = new BufferedReader(r);
+			st = new StringTokenizer(br.readLine());
+		}
+		public String nextLine() throws IOException {
+			return br.readLine();
+		}
+		public boolean hasNext() throws IOException {
+			try {
+				while(!st.hasMoreTokens()) {
+					String s = br.readLine();
+					if(s==null) {
+						return false;
+					}
+					st = new StringTokenizer(s);
+				}
+				return true;
+			}catch (Exception e){
+				return false;
+			}
+		}
+		public String next() throws IOException {
+			if(!hasNext()) {
+				return null;
+			}
+			return st.nextToken();
+		}
+		public int nextInt() throws IOException {
+			String s = next();
+			if(s==null) {
+				return Integer.MIN_VALUE;
+			}
+			return Integer.parseInt(s);
+		}
+		public double nextDouble() throws IOException {
+			String s = next();
+			if(s==null) {
+				return Double.MIN_VALUE;
+			}
+			return Double.parseDouble(s);
+		}
+		public long nextLong() throws IOException {
+			String s = next();
+			if(s==null) {
+				return Long.MIN_VALUE;
+			}
+			return Long.parseLong(s);
+		}
+		public int countTokens() {
+			return st.countTokens();
+		}
+		public void close() throws IOException {
+			br.close();
+		}
+	}
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		Problem_B program = new Problem_B();
+		program.Begin();
+	}
+	void Begin() throws IOException{
+		Input in = new Input(new InputStreamReader(System.in));
+		PrintWriter pw = new PrintWriter(new BufferedOutputStream(System.out));
+		int kase = in.nextInt();
+		while(kase-->0) {
+			int n = in.nextInt();
+			int ans = 1;
+			int[] arr = new int[n];
+			for(int i = 0; i<n; i++) {
+				arr[i] = in.nextInt();
+			}
+			Arrays.sort(arr);
+			int ind = 0;
+			for(; ind<n; ind++) {
+				int cur = 0;
+				for(; ind<n&&arr[ind]>ans+cur; ind++,cur++);
+				if(ind!=n) {
+					ans+=cur+1;
+				}
+			}
+			pw.println(ans);
+		}
+		pw.close();
+		in.close();
+	}
+}
