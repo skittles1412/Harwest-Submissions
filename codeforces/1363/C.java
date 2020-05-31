e273a4509cf1ed9cccb3f6730b22213fb337c40e
skittles1412 <61439840+skittles1412@users.noreply.github.com>
2020-05-31 08:14:00 -0700

    Add solution for problem `C - Game On Leaves` Link: https://codeforces.com/contest/1363/problem/C Tags: games, trees, *1600 Ref: https://codeforces.com/contest/1363/submission/82088238

---
+++ codeforces/1363/C.java
@@ -0,0 +1,98 @@
+//package round_646;
+
+import java.util.*;
+import java.io.*;
+
+public class Problem_C {
+	static class Input {
+		BufferedReader br;
+		StringTokenizer st;
+		public Input(Reader r) throws IOException {
+			br = new BufferedReader(r);
+			st = new StringTokenizer(br.readLine());
+		}
+		public String nextLine() throws IOException {
+			return br.readLine();
+		}
+		public boolean hasNext() throws IOException {
+			try {
+				while(!st.hasMoreTokens()) {
+					String s = br.readLine();
+					if(s==null) {
+						return false;
+					}
+					st = new StringTokenizer(s);
+				}
+				return true;
+			}catch (Exception e){
+				return false;
+			}
+		}
+		public String next() throws IOException {
+			if(!hasNext()) {
+				return null;
+			}
+			return st.nextToken();
+		}
+		public int nextInt() throws IOException {
+			String s = next();
+			if(s==null) {
+				return Integer.MIN_VALUE;
+			}
+			return Integer.parseInt(s);
+		}
+		public double nextDouble() throws IOException {
+			String s = next();
+			if(s==null) {
+				return Double.MIN_VALUE;
+			}
+			return Double.parseDouble(s);
+		}
+		public long nextLong() throws IOException {
+			String s = next();
+			if(s==null) {
+				return Long.MIN_VALUE;
+			}
+			return Long.parseLong(s);
+		}
+		public int countTokens() {
+			return st.countTokens();
+		}
+		public void close() throws IOException {
+			br.close();
+		}
+	}
+	public static void main(String[] args) throws IOException{
+		// TODO Auto-generated method stub
+		Problem_C program = new Problem_C();
+		program.Begin();
+	}
+	void Begin() throws IOException{
+		Input in = new Input(new InputStreamReader(System.in));
+		PrintWriter pw = new PrintWriter(new BufferedOutputStream(System.out));
+		int kase = in.nextInt();
+		while(kase-->0) {
+			int n = in.nextInt();
+			int x = in.nextInt();
+			int degree = 0;
+			for(int i = 0; i<n-1; i++) {
+				int u = in.nextInt();
+				int v = in.nextInt();
+				if(u==x||v==x) {
+					degree++;
+				}
+			}
+			if(degree<=1) {
+				pw.println("Ayush");
+			}else {
+				if(n%2==0) {
+					pw.println("Ayush");
+				}else {
+					pw.println("Ashish");
+				}
+			}
+		}
+		pw.close();
+		in.close();
+	}
+}
