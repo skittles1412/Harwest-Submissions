b6093ea1c73f75aec2ad572db8fd858fc1fdee55
skittles1412 <61439840+skittles1412@users.noreply.github.com>
2020-07-04 19:13:00 -0700

    Add solution for problem `A - Magnets` Link: https://codeforces.com/contest/344/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/344/submission/86018153

---
+++ codeforces/344/A.java
@@ -0,0 +1,83 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.InputStream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+	public static void main(String[] args) {
+		InputStream inputStream = System.in;
+		OutputStream outputStream = System.out;
+		Input in = new Input(inputStream);
+		PrintWriter out = new PrintWriter(outputStream);
+		AMagnets solver = new AMagnets();
+		solver.solve(1, in, out);
+		out.close();
+	}
+	static class AMagnets {
+		public void solve(int testNumber, Input in, PrintWriter out) {
+			int n = in.nextInt(), prev = -1;
+			int ans = 0;
+			for(int i = 0; i<n; i++) {
+				String s = in.next();
+				if(s.charAt(0)!=prev) {
+					ans++;
+					prev = s.charAt(0);
+				}
+			}
+			out.println(ans);
+		}
+
+	}
+
+	static class Input {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public Input(InputStream is) {
+			br = new BufferedReader(new InputStreamReader(is));
+			try {
+				st = new StringTokenizer(br.readLine());
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		public boolean hasNext() {
+			try {
+				while(!st.hasMoreTokens()) {
+					String s = br.readLine();
+					if(s==null) {
+						return false;
+					}
+					st = new StringTokenizer(s);
+				}
+				return true;
+			}catch(Exception e) {
+				return false;
+			}
+		}
+
+		public String next() {
+			if(!hasNext()) {
+				throw new InputMismatchException();
+			}
+			return st.nextToken("\r\n\t\f ");
+		}
+
+		public int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+	}
+}
+
