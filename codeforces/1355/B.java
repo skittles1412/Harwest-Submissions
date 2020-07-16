70841332a628da7a11af71679133182dee0f743d
skittles1412 <61439840+skittles1412@users.noreply.github.com>
2020-07-15 19:31:00 -0700

    Add solution for problem `B - Young Explorers` Link: https://codeforces.com/contest/1355/problem/B Tags: dp, greedy, sortings, *1200 Ref: https://codeforces.com/contest/1355/submission/86951832

---
+++ codeforces/1355/B.java
@@ -0,0 +1,112 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+import java.util.InputMismatchException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.io.InputStream;
+import java.util.ArrayList;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+	static class TaskAdapter implements Runnable {
+		@Override
+		public void run() {
+			InputStream inputStream = System.in;
+			OutputStream outputStream = System.out;
+			Input in = new Input(inputStream);
+			PrintWriter out = new PrintWriter(outputStream);
+			BYoungExplorers solver = new BYoungExplorers();
+			int testCount = Integer.parseInt(in.next());
+			for(int i = 1; i<=testCount; i++)
+				solver.solve(i, in, out);
+			out.close();
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Thread thread = new Thread(null, new TaskAdapter(), "", 1<<29);
+		thread.start();
+		thread.join();
+	}
+	static class BYoungExplorers {
+		public BYoungExplorers() {
+		}
+
+		public void solve(int testNumber, Input in, PrintWriter pw) {
+			int n = in.nextInt(), ans = 0, arr[] = new int[n];
+			for(int i = 0; i<n; i++) {
+				arr[i] = in.nextInt();
+			}
+			Utilities.sort(arr);
+			for(int i = 0; i<n; i++) {
+				int j;
+				for(j = i; j<n&&j-i+1<arr[j]; j++) ;
+				i = j;
+				if(i!=n) {
+					ans++;
+				}
+			}
+			pw.println(ans);
+		}
+
+	}
+
+	static class Utilities {
+		public static void sort(int[] arr) {
+			ArrayList<Integer> al = new ArrayList<Integer>();
+			for(int i: arr) {
+				al.add(i);
+			}
+			Collections.sort(al);
+			for(int i = 0; i<al.size(); i++) {
+				arr[i] = al.get(i);
+			}
+		}
+
+	}
+
+	static class Input {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public Input(InputStream is) {
+			br = new BufferedReader(new InputStreamReader(is), 1<<16);
+			st = null;
+		}
+
+		public boolean hasNext() {
+			try {
+				while(st==null||!st.hasMoreTokens()) {
+					String s = br.readLine();
+					if(s==null) {
+						return false;
+					}
+					st = new StringTokenizer(s);
+				}
+				return true;
+			}catch(Exception e) {
+				return false;
+			}
+		}
+
+		public String next() {
+			if(!hasNext()) {
+				throw new InputMismatchException();
+			}
+			return st.nextToken();
+		}
+
+		public int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+	}
+}
+
