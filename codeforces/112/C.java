d9a98b1087a4e11abb69c54e1d16cef16d57c0ee
skittles1412 <61439840+skittles1412@users.noreply.github.com>
2020-07-19 16:28:00 -0700

    Add solution for problem `C - Petya and Inequiations` Link: https://codeforces.com/contest/112/problem/C Tags: greedy, math, *1400 Ref: https://codeforces.com/contest/112/submission/87388849

---
+++ codeforces/112/C.java
@@ -0,0 +1,174 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Arrays;
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.io.InputStreamReader;
+import java.io.BufferedOutputStream;
+import java.util.StringTokenizer;
+import java.io.Closeable;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.Flushable;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+	static class TaskAdapter implements Runnable {
+		@Override
+		public void run() {
+			InputStream inputStream = System.in;
+			OutputStream outputStream = System.out;
+			Input in = new Input(inputStream);
+			Output out = new Output(outputStream);
+			CPetyaAndInequiations solver = new CPetyaAndInequiations();
+			solver.solve(1, in, out);
+			out.close();
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Thread thread = new Thread(null, new TaskAdapter(), "", 1<<29);
+		thread.start();
+		thread.join();
+	}
+	static class CPetyaAndInequiations {
+		public CPetyaAndInequiations() {
+		}
+
+		public void solve(int testNumber, Input in, Output pw) {
+			int n = in.nextInt();
+			long x = in.nextLong(), y = in.nextLong();
+			long[] arr = new long[n];
+			Arrays.fill(arr, 1);
+			for(; arr[0]*arr[0]+n-1<x; arr[0]++) ;
+			if(arr[0]*arr[0]+n-1<x||arr[0]+n-1>y) {
+				pw.println(-1);
+				return;
+			}
+			pw.println(Utilities.getString(arr, "", "\n", "\n"));
+		}
+
+	}
+
+	static class Utilities {
+		public static String getString(long[] arr, String start, String middle, String end) {
+			StringBuilder builder = new StringBuilder();
+			builder.append(start);
+			for(int i = 0; i<arr.length; i++) {
+				if(i!=0) {
+					builder.append(middle);
+				}
+				builder.append(arr[i]);
+			}
+			builder.append(end);
+			return builder.toString();
+		}
+
+	}
+
+	static class Output implements Closeable, Flushable {
+		public StringBuilder sb;
+		public OutputStream os;
+		public int BUFFER_SIZE;
+		public boolean autoFlush;
+		public String LineSeparator;
+
+		public Output(OutputStream os) {
+			this(os, 1<<16);
+		}
+
+		public Output(OutputStream os, int bs) {
+			BUFFER_SIZE = bs;
+			sb = new StringBuilder(BUFFER_SIZE);
+			this.os = new BufferedOutputStream(os, 1<<17);
+			autoFlush = false;
+			LineSeparator = System.lineSeparator();
+		}
+
+		public void println(int i) {
+			println(String.valueOf(i));
+		}
+
+		public void println(String s) {
+			sb.append(s);
+			println();
+			if(autoFlush||sb.length()>BUFFER_SIZE >> 1) {
+				flush();
+			}
+		}
+
+		public void println() {
+			sb.append(LineSeparator);
+		}
+
+		public void flush() {
+			try {
+				os.write(sb.toString().getBytes());
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+			sb = new StringBuilder(BUFFER_SIZE);
+		}
+
+		public void close() {
+			flush();
+			try {
+				os.close();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	static class Input {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public Input(InputStream is) {
+			this(is, 1<<20);
+		}
+
+		public Input(InputStream is, int bs) {
+			br = new BufferedReader(new InputStreamReader(is), bs);
+			st = null;
+		}
+
+		public boolean hasNext() {
+			try {
+				while(st==null||!st.hasMoreTokens()) {
+					String s = br.readLine();
+					if(s==null) {
+						return false;
+					}
+					st = new StringTokenizer(s);
+				}
+				return true;
+			}catch(Exception e) {
+				return false;
+			}
+		}
+
+		public String next() {
+			if(!hasNext()) {
+				throw new InputMismatchException();
+			}
+			return st.nextToken();
+		}
+
+		public int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		public long nextLong() {
+			return Long.parseLong(next());
+		}
+
+	}
+}
+
