ea92159339e867fe4f72e7b945f880d92beffa4f
skittles1412 <61439840+skittles1412@users.noreply.github.com>
2020-07-19 16:30:00 -0700

    Add solution for problem `B - Lucky String` Link: https://codeforces.com/contest/110/problem/B Tags: constructive algorithms, strings, *1100 Ref: https://codeforces.com/contest/110/submission/87388893

---
+++ codeforces/110/B.java
@@ -0,0 +1,154 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.io.InputStreamReader;
+import java.io.BufferedOutputStream;
+import java.util.StringTokenizer;
+import java.io.Closeable;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.Flushable;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+	static class TaskAdapter implements Runnable {
+		@Override
+		public void run() {
+			InputStream inputStream = System.in;
+			OutputStream outputStream = System.out;
+			Input in = new Input(inputStream);
+			Output out = new Output(outputStream);
+			BLuckyString solver = new BLuckyString();
+			solver.solve(1, in, out);
+			out.close();
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Thread thread = new Thread(null, new TaskAdapter(), "", 1<<29);
+		thread.start();
+		thread.join();
+	}
+	static class BLuckyString {
+		public BLuckyString() {
+		}
+
+		public void solve(int testNumber, Input in, Output pw) {
+			int n = in.nextInt();
+			StringBuilder sb = new StringBuilder();
+			for(int i = 0; i<n; i++) {
+				sb.append((char) (i%4+'a'));
+			}
+			pw.println(sb);
+		}
+
+	}
+
+	static class Input {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public Input(InputStream is) {
+			this(is, 1<<20);
+		}
+
+		public Input(InputStream is, int bs) {
+			br = new BufferedReader(new InputStreamReader(is), bs);
+			st = null;
+		}
+
+		public boolean hasNext() {
+			try {
+				while(st==null||!st.hasMoreTokens()) {
+					String s = br.readLine();
+					if(s==null) {
+						return false;
+					}
+					st = new StringTokenizer(s);
+				}
+				return true;
+			}catch(Exception e) {
+				return false;
+			}
+		}
+
+		public String next() {
+			if(!hasNext()) {
+				throw new InputMismatchException();
+			}
+			return st.nextToken();
+		}
+
+		public int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+	}
+
+	static class Output implements Closeable, Flushable {
+		public StringBuilder sb;
+		public OutputStream os;
+		public int BUFFER_SIZE;
+		public boolean autoFlush;
+		public String LineSeparator;
+
+		public Output(OutputStream os) {
+			this(os, 1<<16);
+		}
+
+		public Output(OutputStream os, int bs) {
+			BUFFER_SIZE = bs;
+			sb = new StringBuilder(BUFFER_SIZE);
+			this.os = new BufferedOutputStream(os, 1<<17);
+			autoFlush = false;
+			LineSeparator = System.lineSeparator();
+		}
+
+		public void print(String s) {
+			sb.append(s);
+			if(autoFlush||sb.length()>BUFFER_SIZE >> 1) {
+				flush();
+			}
+		}
+
+		public void println(Object... o) {
+			for(int i = 0; i<o.length; i++) {
+				if(i!=0) {
+					print(" ");
+				}
+				print(String.valueOf(o[i]));
+			}
+			println();
+		}
+
+		public void println() {
+			sb.append(LineSeparator);
+		}
+
+		public void flush() {
+			try {
+				os.write(sb.toString().getBytes());
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+			sb = new StringBuilder(BUFFER_SIZE);
+		}
+
+		public void close() {
+			flush();
+			try {
+				os.close();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+}
+
