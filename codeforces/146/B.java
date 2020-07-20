bf2851912c739573bf35980c8a752823fa33b3b5
skittles1412 <61439840+skittles1412@users.noreply.github.com>
2020-07-19 20:14:00 -0700

    Add solution for problem `B - Lucky Mask` Link: https://codeforces.com/contest/146/problem/B Tags: brute force, implementation, *1300 Ref: https://codeforces.com/contest/146/submission/87394884

---
+++ codeforces/146/B.java
@@ -0,0 +1,161 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.io.InputStreamReader;
+import java.io.BufferedOutputStream;
+import java.util.StringTokenizer;
+import java.io.Closeable;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.Flushable;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+	static class TaskAdapter implements Runnable {
+		@Override
+		public void run() {
+			InputStream inputStream = System.in;
+			OutputStream outputStream = System.out;
+			Input in = new Input(inputStream);
+			Output out = new Output(outputStream);
+			BLuckyMask solver = new BLuckyMask();
+			solver.solve(1, in, out);
+			out.close();
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Thread thread = new Thread(null, new TaskAdapter(), "", 1<<29);
+		thread.start();
+		thread.join();
+	}
+	static class BLuckyMask {
+		public BLuckyMask() {
+		}
+
+		public boolean valid(int a, int b) {
+			String s = String.valueOf(a);
+			StringBuilder sb = new StringBuilder();
+			for(int i = 0; i<s.length(); i++) {
+				if(s.charAt(i)=='4'||s.charAt(i)=='7') {
+					sb.append(s.charAt(i));
+				}
+			}
+			return sb.length()!=0&&Integer.parseInt(sb.toString())==b;
+		}
+
+		public void solve(int testNumber, Input in, Output pw) {
+			int a = in.nextInt(), b = in.nextInt(), i;
+			for(i = a+1; ; i++) {
+				if(valid(i, b)) {
+					pw.println(i);
+					break;
+				}
+			}
+		}
+
+	}
+
+	static class Output implements Closeable, Flushable {
+		public StringBuilder sb;
+		public OutputStream os;
+		public int BUFFER_SIZE;
+		public boolean autoFlush;
+		public String LineSeparator;
+
+		public Output(OutputStream os) {
+			this(os, 1<<16);
+		}
+
+		public Output(OutputStream os, int bs) {
+			BUFFER_SIZE = bs;
+			sb = new StringBuilder(BUFFER_SIZE);
+			this.os = new BufferedOutputStream(os, 1<<17);
+			autoFlush = false;
+			LineSeparator = System.lineSeparator();
+		}
+
+		public void println(int i) {
+			println(String.valueOf(i));
+		}
+
+		public void println(String s) {
+			sb.append(s);
+			println();
+			if(autoFlush||sb.length()>BUFFER_SIZE >> 1) {
+				flush();
+			}
+		}
+
+		public void println() {
+			sb.append(LineSeparator);
+		}
+
+		public void flush() {
+			try {
+				os.write(sb.toString().getBytes());
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+			sb = new StringBuilder(BUFFER_SIZE);
+		}
+
+		public void close() {
+			flush();
+			try {
+				os.close();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	static class Input {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public Input(InputStream is) {
+			this(is, 1<<20);
+		}
+
+		public Input(InputStream is, int bs) {
+			br = new BufferedReader(new InputStreamReader(is), bs);
+			st = null;
+		}
+
+		public boolean hasNext() {
+			try {
+				while(st==null||!st.hasMoreTokens()) {
+					String s = br.readLine();
+					if(s==null) {
+						return false;
+					}
+					st = new StringTokenizer(s);
+				}
+				return true;
+			}catch(Exception e) {
+				return false;
+			}
+		}
+
+		public String next() {
+			if(!hasNext()) {
+				throw new InputMismatchException();
+			}
+			return st.nextToken();
+		}
+
+		public int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+	}
+}
+
