b3f0deb8d8ebe83d33d713afa418dfd04a1aec12
skittles1412 <61439840+skittles1412@users.noreply.github.com>
2020-07-21 21:03:00 -0700

    Add solution for problem `C - Pride` Link: https://codeforces.com/contest/892/problem/C Tags: greedy, *1500 Ref: https://codeforces.com/contest/892/submission/87630487

---
+++ codeforces/892/C.java
@@ -0,0 +1,194 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.io.InputStreamReader;
+import java.io.BufferedOutputStream;
+import java.util.StringTokenizer;
+import java.io.Closeable;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.Flushable;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+	static class TaskAdapter implements Runnable {
+		@Override
+		public void run() {
+			InputStream inputStream = System.in;
+			OutputStream outputStream = System.out;
+			Input in = new Input(inputStream);
+			Output out = new Output(outputStream);
+			CPride solver = new CPride();
+			solver.solve(1, in, out);
+			out.close();
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Thread thread = new Thread(null, new TaskAdapter(), "", 1<<29);
+		thread.start();
+		thread.join();
+	}
+	static class CPride {
+		public CPride() {
+		}
+
+		public void solve(int testNumber, Input in, Output pw) {
+			int n = in.nextInt();
+			int[] arr = new int[n];
+			for(int i = 0; i<n; i++) {
+				arr[i] = in.nextInt();
+			}
+			int gcd = arr[0];
+			for(int i = 1; i<n; i++) {
+				gcd = Utilities.gcd(gcd, arr[i]);
+			}
+			if(gcd!=1) {
+				pw.println(-1);
+				return;
+			}
+			int min = Integer.MAX_VALUE, cnt = 0;
+			for(int i = 0; i<n; i++) {
+				if(arr[i]==1) {
+					cnt++;
+				}
+			}
+			if(cnt>0) {
+				pw.println(n-cnt);
+				return;
+			}
+			for(int i = 0; i<n; i++) {
+				gcd = arr[i];
+				for(int j = i; j<n&&j-i<min; j++) {
+					gcd = Utilities.gcd(gcd, arr[j]);
+					if(gcd==1) {
+						min = Math.min(min, j-i);
+					}
+				}
+			}
+			pw.println(min+n-1);
+		}
+
+	}
+
+	static class Output implements Closeable, Flushable {
+		public StringBuilder sb;
+		public OutputStream os;
+		public int BUFFER_SIZE;
+		public boolean autoFlush;
+		public String LineSeparator;
+
+		public Output(OutputStream os) {
+			this(os, 1<<16);
+		}
+
+		public Output(OutputStream os, int bs) {
+			BUFFER_SIZE = bs;
+			sb = new StringBuilder(BUFFER_SIZE);
+			this.os = new BufferedOutputStream(os, 1<<17);
+			autoFlush = false;
+			LineSeparator = System.lineSeparator();
+		}
+
+		public void println(int i) {
+			println(String.valueOf(i));
+		}
+
+		public void println(String s) {
+			sb.append(s);
+			println();
+			if(autoFlush) {
+				flush();
+			}else if(sb.length()>BUFFER_SIZE >> 1) {
+				flushToBuffer();
+			}
+		}
+
+		public void println() {
+			sb.append(LineSeparator);
+		}
+
+		private void flushToBuffer() {
+			try {
+				os.write(sb.toString().getBytes());
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+			sb = new StringBuilder(BUFFER_SIZE);
+		}
+
+		public void flush() {
+			try {
+				flushToBuffer();
+				os.flush();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		public void close() {
+			flush();
+			try {
+				os.close();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	static class Input {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public Input(InputStream is) {
+			this(is, 1<<20);
+		}
+
+		public Input(InputStream is, int bs) {
+			br = new BufferedReader(new InputStreamReader(is), bs);
+			st = null;
+		}
+
+		public boolean hasNext() {
+			try {
+				while(st==null||!st.hasMoreTokens()) {
+					String s = br.readLine();
+					if(s==null) {
+						return false;
+					}
+					st = new StringTokenizer(s);
+				}
+				return true;
+			}catch(Exception e) {
+				return false;
+			}
+		}
+
+		public String next() {
+			if(!hasNext()) {
+				throw new InputMismatchException();
+			}
+			return st.nextToken();
+		}
+
+		public int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+	}
+
+	static class Utilities {
+		public static int gcd(int a, int b) {
+			return b==0 ? a : gcd(b, a%b);
+		}
+
+	}
+}
+
