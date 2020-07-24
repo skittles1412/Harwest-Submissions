9d6b8cee2c42f80bafcd280f9e1c8d26cf8307cb
skittles1412 <61439840+skittles1412@users.noreply.github.com>
2020-07-23 17:02:00 -0700

    Add solution for problem `D - Antimatter` Link: https://codeforces.com/contest/383/problem/D Tags: dp, *2300 Ref: https://codeforces.com/contest/383/submission/87815900

---
+++ codeforces/383/D.java
@@ -0,0 +1,184 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Arrays;
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.io.InputStreamReader;
+import java.io.BufferedOutputStream;
+import java.util.StringTokenizer;
+import java.io.Closeable;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.Flushable;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+	static class TaskAdapter implements Runnable {
+		@Override
+		public void run() {
+			InputStream inputStream = System.in;
+			OutputStream outputStream = System.out;
+			Input in = new Input(inputStream);
+			Output out = new Output(outputStream);
+			DAntimatter solver = new DAntimatter();
+			solver.solve(1, in, out);
+			out.close();
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Thread thread = new Thread(null, new TaskAdapter(), "", 1<<29);
+		thread.start();
+		thread.join();
+	}
+	static class DAntimatter {
+		int n;
+		int[] arr;
+		int[][] memo;
+
+		public DAntimatter() {
+		}
+
+		public int dp(int ind, int sum) {
+			if(ind==-1) {
+				return sum==0 ? 1 : 0;
+			}else if(memo[ind][sum+20000]!=-1) {
+				return memo[ind][sum+20000];
+			}
+			long ans = (dp(ind-1, sum-arr[ind])+dp(ind-1, sum+arr[ind])+(sum==0 ? 1 : 0))%1_000_000_007L;
+			return memo[ind][sum+20000] = (int) ans;
+		}
+
+		public void solve(int testNumber, Input in, Output pw) {
+			n = in.nextInt();
+			arr = new int[n];
+			for(int i = 0; i<n; i++) {
+				arr[i] = in.nextInt();
+			}
+			memo = new int[n][30000];
+			for(int i = 0; i<n; i++) {
+				Arrays.fill(memo[i], -1);
+			}
+			long ans = 0;
+			for(int i = 0; i<n; i++) {
+				ans += dp(i, 0)-1;
+				ans %= 1_000_000_007L;
+			}
+			pw.println(ans);
+		}
+
+	}
+
+	static class Output implements Closeable, Flushable {
+		public StringBuilder sb;
+		public OutputStream os;
+		public int BUFFER_SIZE;
+		public boolean autoFlush;
+		public String LineSeparator;
+
+		public Output(OutputStream os) {
+			this(os, 1<<16);
+		}
+
+		public Output(OutputStream os, int bs) {
+			BUFFER_SIZE = bs;
+			sb = new StringBuilder(BUFFER_SIZE);
+			this.os = new BufferedOutputStream(os, 1<<17);
+			autoFlush = false;
+			LineSeparator = System.lineSeparator();
+		}
+
+		public void println(long l) {
+			println(String.valueOf(l));
+		}
+
+		public void println(String s) {
+			sb.append(s);
+			println();
+			if(autoFlush) {
+				flush();
+			}else if(sb.length()>BUFFER_SIZE >> 1) {
+				flushToBuffer();
+			}
+		}
+
+		public void println() {
+			sb.append(LineSeparator);
+		}
+
+		private void flushToBuffer() {
+			try {
+				os.write(sb.toString().getBytes());
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+			sb = new StringBuilder(BUFFER_SIZE);
+		}
+
+		public void flush() {
+			try {
+				flushToBuffer();
+				os.flush();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		public void close() {
+			flush();
+			try {
+				os.close();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	static class Input {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public Input(InputStream is) {
+			this(is, 1<<20);
+		}
+
+		public Input(InputStream is, int bs) {
+			br = new BufferedReader(new InputStreamReader(is), bs);
+			st = null;
+		}
+
+		public boolean hasNext() {
+			try {
+				while(st==null||!st.hasMoreTokens()) {
+					String s = br.readLine();
+					if(s==null) {
+						return false;
+					}
+					st = new StringTokenizer(s);
+				}
+				return true;
+			}catch(Exception e) {
+				return false;
+			}
+		}
+
+		public String next() {
+			if(!hasNext()) {
+				throw new InputMismatchException();
+			}
+			return st.nextToken();
+		}
+
+		public int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+	}
+}
+
