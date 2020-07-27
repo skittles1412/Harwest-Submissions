e3ff726d6069b6c7c7253052a9edd9fc67259078
skittles1412 <61439840+skittles1412@users.noreply.github.com>
2020-07-27 15:33:00 -0700

    Add solution for problem `C - Classroom Watch` Link: https://codeforces.com/contest/876/problem/C Tags: brute force, math, *1200 Ref: https://codeforces.com/contest/876/submission/88161069

---
+++ codeforces/876/C.java
@@ -0,0 +1,193 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.io.BufferedOutputStream;
+import java.util.StringTokenizer;
+import java.io.Closeable;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.Flushable;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+	static class TaskAdapter implements Runnable {
+		@Override
+		public void run() {
+			InputStream inputStream = System.in;
+			OutputStream outputStream = System.out;
+			Input in = new Input(inputStream);
+			Output out = new Output(outputStream);
+			CClassroomWatch solver = new CClassroomWatch();
+			solver.solve(1, in, out);
+			out.close();
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Thread thread = new Thread(null, new TaskAdapter(), "", 1<<29);
+		thread.start();
+		thread.join();
+	}
+	static class CClassroomWatch {
+		public CClassroomWatch() {
+		}
+
+		public int sum(int x) {
+			String s = String.valueOf(x);
+			int ret = 0;
+			for(int i = 0; i<s.length(); i++) {
+				ret += s.charAt(i)-'0';
+			}
+			return ret;
+		}
+
+		public void solve(int kase, Input in, Output pw) {
+			int n = in.nextInt();
+			ArrayList<Integer> ans = new ArrayList<>();
+			for(int i = Math.max(n-100, 0); i<=n; i++) {
+				if(i+sum(i)==n) {
+					ans.add(i);
+				}
+			}
+			pw.println(ans.size());
+			if(!ans.isEmpty()) {
+				pw.println(Utilities.getString(ans, "", "\n", ""));
+			}
+		}
+
+	}
+
+	static class Output implements Closeable, Flushable {
+		public StringBuilder sb;
+		public OutputStream os;
+		public int BUFFER_SIZE;
+		public boolean autoFlush;
+		public String LineSeparator;
+
+		public Output(OutputStream os) {
+			this(os, 1<<16);
+		}
+
+		public Output(OutputStream os, int bs) {
+			BUFFER_SIZE = bs;
+			sb = new StringBuilder(BUFFER_SIZE);
+			this.os = new BufferedOutputStream(os, 1<<17);
+			autoFlush = false;
+			LineSeparator = System.lineSeparator();
+		}
+
+		public void println(int i) {
+			println(String.valueOf(i));
+		}
+
+		public void println(String s) {
+			sb.append(s);
+			println();
+			if(autoFlush) {
+				flush();
+			}else if(sb.length()>BUFFER_SIZE >> 1) {
+				flushToBuffer();
+			}
+		}
+
+		public void println() {
+			sb.append(LineSeparator);
+		}
+
+		private void flushToBuffer() {
+			try {
+				os.write(sb.toString().getBytes());
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+			sb = new StringBuilder(BUFFER_SIZE);
+		}
+
+		public void flush() {
+			try {
+				flushToBuffer();
+				os.flush();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		public void close() {
+			flush();
+			try {
+				os.close();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	static class Utilities {
+		public static <T> String getString(Iterable<T> iter, String start, String middle, String end) {
+			StringBuilder builder = new StringBuilder();
+			builder.append(start);
+			boolean first = true;
+			for(T t: iter) {
+				if(!first) {
+					builder.append(middle);
+				}
+				first = false;
+				builder.append(t.toString());
+			}
+			builder.append(end);
+			return builder.toString();
+		}
+
+	}
+
+	static class Input {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public Input(InputStream is) {
+			this(is, 1<<20);
+		}
+
+		public Input(InputStream is, int bs) {
+			br = new BufferedReader(new InputStreamReader(is), bs);
+			st = null;
+		}
+
+		public boolean hasNext() {
+			try {
+				while(st==null||!st.hasMoreTokens()) {
+					String s = br.readLine();
+					if(s==null) {
+						return false;
+					}
+					st = new StringTokenizer(s);
+				}
+				return true;
+			}catch(Exception e) {
+				return false;
+			}
+		}
+
+		public String next() {
+			if(!hasNext()) {
+				throw new InputMismatchException();
+			}
+			return st.nextToken();
+		}
+
+		public int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+	}
+}
+
