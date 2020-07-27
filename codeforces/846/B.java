c8f2b4c42683187b7303543b36f241ab6aa7ed50
skittles1412 <61439840+skittles1412@users.noreply.github.com>
2020-07-27 10:52:00 -0700

    Add solution for problem `B - Math Show` Link: https://codeforces.com/contest/846/problem/B Tags: brute force, greedy, *1800 Ref: https://codeforces.com/contest/846/submission/88150006

---
+++ codeforces/846/B.java
@@ -0,0 +1,195 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.io.BufferedOutputStream;
+import java.util.StringTokenizer;
+import java.io.Closeable;
+import java.io.BufferedReader;
+import java.util.Collections;
+import java.io.InputStream;
+import java.io.Flushable;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+	static class TaskAdapter implements Runnable {
+		@Override
+		public void run() {
+			InputStream inputStream = System.in;
+			OutputStream outputStream = System.out;
+			Input in = new Input(inputStream);
+			Output out = new Output(outputStream);
+			BMathShow solver = new BMathShow();
+			solver.solve(1, in, out);
+			out.close();
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Thread thread = new Thread(null, new TaskAdapter(), "", 1<<29);
+		thread.start();
+		thread.join();
+	}
+	static class BMathShow {
+		public BMathShow() {
+		}
+
+		public void solve(int kase, Input in, Output pw) {
+			int n = in.nextInt(), m = in.nextInt(), t = in.nextInt(), sum = 0;
+			int[] arr = new int[m];
+			for(int i = 0; i<m; i++) {
+				sum += arr[i] = in.nextInt();
+			}
+			Utilities.sort(arr);
+			int ans = 0;
+			for(int i = 0; i<=n; i++) {
+				int left = t-i*sum, cur = (m+1)*i;
+				if(left<0) {
+					break;
+				}
+				loop:
+				for(int j = 0; j<m; j++) {
+					for(int k = 0; k<n-i; k++) {
+						left -= arr[j];
+						if(left<0) {
+							break loop;
+						}
+						cur++;
+					}
+				}
+				ans = Math.max(ans, cur);
+			}
+			pw.println(ans);
+		}
+
+	}
+
+	static class Output implements Closeable, Flushable {
+		public StringBuilder sb;
+		public OutputStream os;
+		public int BUFFER_SIZE;
+		public boolean autoFlush;
+		public String LineSeparator;
+
+		public Output(OutputStream os) {
+			this(os, 1<<16);
+		}
+
+		public Output(OutputStream os, int bs) {
+			BUFFER_SIZE = bs;
+			sb = new StringBuilder(BUFFER_SIZE);
+			this.os = new BufferedOutputStream(os, 1<<17);
+			autoFlush = false;
+			LineSeparator = System.lineSeparator();
+		}
+
+		public void println(int i) {
+			println(String.valueOf(i));
+		}
+
+		public void println(String s) {
+			sb.append(s);
+			println();
+			if(autoFlush) {
+				flush();
+			}else if(sb.length()>BUFFER_SIZE >> 1) {
+				flushToBuffer();
+			}
+		}
+
+		public void println() {
+			sb.append(LineSeparator);
+		}
+
+		private void flushToBuffer() {
+			try {
+				os.write(sb.toString().getBytes());
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+			sb = new StringBuilder(BUFFER_SIZE);
+		}
+
+		public void flush() {
+			try {
+				flushToBuffer();
+				os.flush();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		public void close() {
+			flush();
+			try {
+				os.close();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	static class Utilities {
+		public static void sort(int[] arr) {
+			ArrayList<Integer> al = new ArrayList<>();
+			for(int i: arr) {
+				al.add(i);
+			}
+			Collections.sort(al);
+			for(int i = 0; i<al.size(); i++) {
+				arr[i] = al.get(i);
+			}
+		}
+
+	}
+
+	static class Input {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public Input(InputStream is) {
+			this(is, 1<<20);
+		}
+
+		public Input(InputStream is, int bs) {
+			br = new BufferedReader(new InputStreamReader(is), bs);
+			st = null;
+		}
+
+		public boolean hasNext() {
+			try {
+				while(st==null||!st.hasMoreTokens()) {
+					String s = br.readLine();
+					if(s==null) {
+						return false;
+					}
+					st = new StringTokenizer(s);
+				}
+				return true;
+			}catch(Exception e) {
+				return false;
+			}
+		}
+
+		public String next() {
+			if(!hasNext()) {
+				throw new InputMismatchException();
+			}
+			return st.nextToken();
+		}
+
+		public int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+	}
+}
+
