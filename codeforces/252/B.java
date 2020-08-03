1d67cbd64492da1adb482fb34a64abfd3f896b19
skittles1412 <61439840+skittles1412@users.noreply.github.com>
2020-08-02 20:58:00 -0700

    Add solution for problem `B - Unsorting Array` Link: https://codeforces.com/contest/252/problem/B Tags: brute force, sortings, *1800 Ref: https://codeforces.com/contest/252/submission/88770403

---
+++ codeforces/252/B.java
@@ -0,0 +1,221 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.io.InputStreamReader;
+import java.io.BufferedOutputStream;
+import java.util.StringTokenizer;
+import java.io.Closeable;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.Flushable;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+	static class TaskAdapter implements Runnable {
+		@Override
+		public void run() {
+			InputStream inputStream = System.in;
+			OutputStream outputStream = System.out;
+			Input in = new Input(inputStream);
+			Output out = new Output(outputStream);
+			BUnsortingArray solver = new BUnsortingArray();
+			solver.solve(1, in, out);
+			out.close();
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Thread thread = new Thread(null, new TaskAdapter(), "", 1<<29);
+		thread.start();
+		thread.join();
+	}
+	static class BUnsortingArray {
+		public BUnsortingArray() {
+		}
+
+		public boolean unsorted(int[] arr) {
+			boolean valid = true;
+			for(int i = 0; i<arr.length-1&&valid; i++) {
+				valid = arr[i]<=arr[i+1];
+			}
+			if(valid) {
+				return false;
+			}
+			valid = true;
+			for(int i = 0; i<arr.length-1&&valid; i++) {
+				valid = arr[i]>=arr[i+1];
+			}
+			return !valid;
+		}
+
+		public void solve(int kase, Input in, Output pw) {
+			int n = in.nextInt();
+			int[] arr = new int[n];
+			for(int i = 0; i<n; i++) {
+				arr[i] = in.nextInt();
+			}
+			boolean equals = true;
+			for(int i = 0; i<n-1&&equals; i++) {
+				equals = arr[i]==arr[i+1];
+			}
+			if(equals) {
+				pw.println("-1");
+				return;
+			}
+			for(int i = 0; i<n; i++) {
+				for(int j = i+1; j<n; j++) {
+					if(arr[i]!=arr[j]) {
+						Utilities.swap(arr, i, j);
+						if(unsorted(arr)) {
+							pw.println(i+1, j+1);
+							return;
+						}
+						Utilities.swap(arr, i, j);
+					}
+				}
+			}
+			pw.println("-1");
+		}
+
+	}
+
+	static class Input {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public Input(InputStream is) {
+			this(is, 1<<20);
+		}
+
+		public Input(InputStream is, int bs) {
+			br = new BufferedReader(new InputStreamReader(is), bs);
+			st = null;
+		}
+
+		public boolean hasNext() {
+			try {
+				while(st==null||!st.hasMoreTokens()) {
+					String s = br.readLine();
+					if(s==null) {
+						return false;
+					}
+					st = new StringTokenizer(s);
+				}
+				return true;
+			}catch(Exception e) {
+				return false;
+			}
+		}
+
+		public String next() {
+			if(!hasNext()) {
+				throw new InputMismatchException();
+			}
+			return st.nextToken();
+		}
+
+		public int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+	}
+
+	static class Utilities {
+		public static void swap(int[] arr, int i, int j) {
+			if(i!=j) {
+				arr[i] ^= arr[j];
+				arr[j] ^= arr[i];
+				arr[i] ^= arr[j];
+			}
+		}
+
+	}
+
+	static class Output implements Closeable, Flushable {
+		public StringBuilder sb;
+		public OutputStream os;
+		public int BUFFER_SIZE;
+		public boolean autoFlush;
+		public String LineSeparator;
+
+		public Output(OutputStream os) {
+			this(os, 1<<16);
+		}
+
+		public Output(OutputStream os, int bs) {
+			BUFFER_SIZE = bs;
+			sb = new StringBuilder(BUFFER_SIZE);
+			this.os = new BufferedOutputStream(os, 1<<17);
+			autoFlush = false;
+			LineSeparator = System.lineSeparator();
+		}
+
+		public void print(String s) {
+			sb.append(s);
+			if(autoFlush) {
+				flush();
+			}else if(sb.length()>BUFFER_SIZE >> 1) {
+				flushToBuffer();
+			}
+		}
+
+		public void println(Object... o) {
+			for(int i = 0; i<o.length; i++) {
+				if(i!=0) {
+					print(" ");
+				}
+				print(String.valueOf(o[i]));
+			}
+			println();
+		}
+
+		public void println(String s) {
+			sb.append(s);
+			println();
+			if(autoFlush) {
+				flush();
+			}else if(sb.length()>BUFFER_SIZE >> 1) {
+				flushToBuffer();
+			}
+		}
+
+		public void println() {
+			sb.append(LineSeparator);
+		}
+
+		private void flushToBuffer() {
+			try {
+				os.write(sb.toString().getBytes());
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+			sb = new StringBuilder(BUFFER_SIZE);
+		}
+
+		public void flush() {
+			try {
+				flushToBuffer();
+				os.flush();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		public void close() {
+			flush();
+			try {
+				os.close();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+}
+
