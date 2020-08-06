d2aa62506ca5b06d3601c74eecebbbe305a874d6
skittles1412 <61439840+skittles1412@users.noreply.github.com>
2020-08-05 19:58:00 -0700

    Add solution for problem `B - z-sort` Link: https://codeforces.com/contest/652/problem/B Tags: sortings, *1000 Ref: https://codeforces.com/contest/652/submission/89081865

---
+++ codeforces/652/B.java
@@ -0,0 +1,201 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Arrays;
+import java.io.IOException;
+import java.util.Random;
+import java.util.InputMismatchException;
+import java.io.InputStreamReader;
+import java.io.BufferedOutputStream;
+import java.util.StringTokenizer;
+import java.io.Closeable;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.Flushable;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+	static class TaskAdapter implements Runnable {
+		@Override
+		public void run() {
+			InputStream inputStream = System.in;
+			OutputStream outputStream = System.out;
+			Input in = new Input(inputStream);
+			Output out = new Output(outputStream);
+			BZSort solver = new BZSort();
+			solver.solve(1, in, out);
+			out.close();
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Thread thread = new Thread(null, new TaskAdapter(), "", 1<<29);
+		thread.start();
+		thread.join();
+	}
+	static class BZSort {
+		public BZSort() {
+		}
+
+		public void solve(int kase, Input in, Output pw) {
+			int n = in.nextInt();
+			int[] arr = new int[n];
+			int[] ans = new int[n];
+			for(int i = 0; i<n; i++) {
+				arr[i] = in.nextInt();
+			}
+			Utilities.sort(arr);
+			int ind = 0;
+			for(int i = 0; i<n; i += 2) {
+				ans[i] = arr[ind++];
+			}
+			for(int i = 1; i<n; i += 2) {
+				ans[i] = arr[ind++];
+			}
+			pw.println(ans);
+		}
+
+	}
+
+	static class Output implements Closeable, Flushable {
+		public StringBuilder sb;
+		public OutputStream os;
+		public int BUFFER_SIZE;
+		public boolean autoFlush;
+		public String LineSeparator;
+
+		public Output(OutputStream os) {
+			this(os, 1<<16);
+		}
+
+		public Output(OutputStream os, int bs) {
+			BUFFER_SIZE = bs;
+			sb = new StringBuilder(BUFFER_SIZE);
+			this.os = new BufferedOutputStream(os, 1<<17);
+			autoFlush = false;
+			LineSeparator = System.lineSeparator();
+		}
+
+		public void print(int i) {
+			print(String.valueOf(i));
+		}
+
+		public void print(String s) {
+			sb.append(s);
+			if(autoFlush) {
+				flush();
+			}else if(sb.length()>BUFFER_SIZE >> 1) {
+				flushToBuffer();
+			}
+		}
+
+		public void println() {
+			sb.append(LineSeparator);
+		}
+
+		public void println(int[] arr) {
+			for(int i = 0; i<arr.length; i++) {
+				if(i!=0) {
+					print(" ");
+				}
+				print(arr[i]);
+			}
+			println();
+		}
+
+		private void flushToBuffer() {
+			try {
+				os.write(sb.toString().getBytes());
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+			sb = new StringBuilder(BUFFER_SIZE);
+		}
+
+		public void flush() {
+			try {
+				flushToBuffer();
+				os.flush();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		public void close() {
+			flush();
+			try {
+				os.close();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	static class Utilities {
+		public static void sort(int[] arr) {
+			Random rand = new Random();
+			int n = arr.length;
+			for(int i = 0; i<n; i++) {
+				swap(arr, i, rand.nextInt(n));
+			}
+			Arrays.sort(arr);
+		}
+
+		public static void swap(int[] arr, int i, int j) {
+			if(i!=j) {
+				arr[i] ^= arr[j];
+				arr[j] ^= arr[i];
+				arr[i] ^= arr[j];
+			}
+		}
+
+	}
+
+	static class Input {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public Input(InputStream is) {
+			this(is, 1<<20);
+		}
+
+		public Input(InputStream is, int bs) {
+			br = new BufferedReader(new InputStreamReader(is), bs);
+			st = null;
+		}
+
+		public boolean hasNext() {
+			try {
+				while(st==null||!st.hasMoreTokens()) {
+					String s = br.readLine();
+					if(s==null) {
+						return false;
+					}
+					st = new StringTokenizer(s);
+				}
+				return true;
+			}catch(Exception e) {
+				return false;
+			}
+		}
+
+		public String next() {
+			if(!hasNext()) {
+				throw new InputMismatchException();
+			}
+			return st.nextToken();
+		}
+
+		public int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+	}
+}
+
+
