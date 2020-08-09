1f4bb431cf4b5a1b3ae14f29cc91de9982106dda
skittles1412 <61439840+skittles1412@users.noreply.github.com>
2020-08-09 07:11:00 -0700

    Add solution for problem `A - Lucky Division` Link: https://codeforces.com/contest/122/problem/A Tags: brute force, number theory, *1000 Ref: https://codeforces.com/contest/122/submission/89405346

---
+++ codeforces/122/A.java
@@ -0,0 +1,171 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.io.InputStreamReader;
+import java.io.BufferedOutputStream;
+import java.util.StringTokenizer;
+import java.io.Closeable;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.Flushable;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+	static class TaskAdapter implements Runnable {
+		@Override
+		public void run() {
+			InputStream inputStream = System.in;
+			OutputStream outputStream = System.out;
+			Input in = new Input(inputStream);
+			Output out = new Output(outputStream);
+			ALuckyDivision solver = new ALuckyDivision();
+			solver.solve(1, in, out);
+			out.close();
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Thread thread = new Thread(null, new TaskAdapter(), "", 1<<29);
+		thread.start();
+		thread.join();
+	}
+	static class ALuckyDivision {
+		public ALuckyDivision() {
+		}
+
+		public void solve(int kase, Input in, Output pw) {
+			int n = in.nextInt();
+			for(int len = 1; len<=3; len++) {
+				for(int i = 0; i<1<<len; i++) {
+					int num = 0;
+					for(int j = 0; j<len; j++) {
+						if((i&1<<j)>0) {
+							num += 4;
+						}else {
+							num += 7;
+						}
+						num *= 10;
+					}
+					num /= 10;
+					if(n%num==0) {
+						pw.println("YES");
+						return;
+					}
+				}
+			}
+			pw.println("NO");
+		}
+
+	}
+
+	static class Input {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public Input(InputStream is) {
+			this(is, 1<<20);
+		}
+
+		public Input(InputStream is, int bs) {
+			br = new BufferedReader(new InputStreamReader(is), bs);
+			st = null;
+		}
+
+		public boolean hasNext() {
+			try {
+				while(st==null||!st.hasMoreTokens()) {
+					String s = br.readLine();
+					if(s==null) {
+						return false;
+					}
+					st = new StringTokenizer(s);
+				}
+				return true;
+			}catch(Exception e) {
+				return false;
+			}
+		}
+
+		public String next() {
+			if(!hasNext()) {
+				throw new InputMismatchException();
+			}
+			return st.nextToken();
+		}
+
+		public int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+	}
+
+	static class Output implements Closeable, Flushable {
+		public StringBuilder sb;
+		public OutputStream os;
+		public int BUFFER_SIZE;
+		public boolean autoFlush;
+		public String LineSeparator;
+
+		public Output(OutputStream os) {
+			this(os, 1<<16);
+		}
+
+		public Output(OutputStream os, int bs) {
+			BUFFER_SIZE = bs;
+			sb = new StringBuilder(BUFFER_SIZE);
+			this.os = new BufferedOutputStream(os, 1<<17);
+			autoFlush = false;
+			LineSeparator = System.lineSeparator();
+		}
+
+		public void println(String s) {
+			sb.append(s);
+			println();
+			if(autoFlush) {
+				flush();
+			}else if(sb.length()>BUFFER_SIZE >> 1) {
+				flushToBuffer();
+			}
+		}
+
+		public void println() {
+			sb.append(LineSeparator);
+		}
+
+		private void flushToBuffer() {
+			try {
+				os.write(sb.toString().getBytes());
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+			sb = new StringBuilder(BUFFER_SIZE);
+		}
+
+		public void flush() {
+			try {
+				flushToBuffer();
+				os.flush();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		public void close() {
+			flush();
+			try {
+				os.close();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+}
+
+
