735373a8ad483ba0554721f0d426267c61678b7a
skittles1412 <61439840+skittles1412@users.noreply.github.com>
2020-08-14 07:43:00 -0700

    Add solution for problem `B - Substring Removal Game` Link: https://codeforces.com/contest/1398/problem/B Tags: games, greedy, sortings, *800 Ref: https://codeforces.com/contest/1398/submission/89889016

---
+++ codeforces/1398/B.java
@@ -0,0 +1,183 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.io.InputStreamReader;
+import java.io.BufferedOutputStream;
+import java.util.StringTokenizer;
+import java.io.Closeable;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.Flushable;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+	static class TaskAdapter implements Runnable {
+		@Override
+		public void run() {
+			InputStream inputStream = System.in;
+			OutputStream outputStream = System.out;
+			Input in = new Input(inputStream);
+			Output out = new Output(outputStream);
+			BSubstringRemovalGame solver = new BSubstringRemovalGame();
+			int testCount = Integer.parseInt(in.next());
+			for(int i = 1; i<=testCount; i++)
+				solver.solve(i, in, out);
+			out.close();
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Thread thread = new Thread(null, new TaskAdapter(), "", 1<<29);
+		thread.start();
+		thread.join();
+	}
+	static class BSubstringRemovalGame {
+		public BSubstringRemovalGame() {
+		}
+
+		public String delete(String s, int i, int j) {
+			return s.substring(0, i)+s.substring(j+1);
+		}
+
+		public void solve(int kase, Input in, Output pw) {
+			String s = in.next();
+			int ans = 0, cnt = 0;
+			while(s.length()!=0) {
+				int max = 0, start = -1, end = -1;
+				for(int i = 0; i<s.length(); ) {
+					for(; i<s.length()&&s.charAt(i)=='0'; i++) ;
+					int x = i;
+					for(; i<s.length()&&s.charAt(i)=='1'; i++) ;
+					if(x!=s.length()) {
+						int cs = x, ce = i-1, tot = ce-cs+1;
+						if(tot>max) {
+							max = tot;
+							start = cs;
+							end = ce;
+						}
+					}
+				}
+				if(start==-1) {
+					break;
+				}
+				if((cnt++&1)==0) {
+					ans += max;
+				}
+				s = delete(s, start, end);
+			}
+			pw.println(ans);
+		}
+
+	}
+
+	static class Input {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public Input(InputStream is) {
+			this(is, 1<<20);
+		}
+
+		public Input(InputStream is, int bs) {
+			br = new BufferedReader(new InputStreamReader(is), bs);
+			st = null;
+		}
+
+		public boolean hasNext() {
+			try {
+				while(st==null||!st.hasMoreTokens()) {
+					String s = br.readLine();
+					if(s==null) {
+						return false;
+					}
+					st = new StringTokenizer(s);
+				}
+				return true;
+			}catch(Exception e) {
+				return false;
+			}
+		}
+
+		public String next() {
+			if(!hasNext()) {
+				throw new InputMismatchException();
+			}
+			return st.nextToken();
+		}
+
+	}
+
+	static class Output implements Closeable, Flushable {
+		public StringBuilder sb;
+		public OutputStream os;
+		public int BUFFER_SIZE;
+		public boolean autoFlush;
+		public String LineSeparator;
+
+		public Output(OutputStream os) {
+			this(os, 1<<16);
+		}
+
+		public Output(OutputStream os, int bs) {
+			BUFFER_SIZE = bs;
+			sb = new StringBuilder(BUFFER_SIZE);
+			this.os = new BufferedOutputStream(os, 1<<17);
+			autoFlush = false;
+			LineSeparator = System.lineSeparator();
+		}
+
+		public void println(int i) {
+			println(String.valueOf(i));
+		}
+
+		public void println(String s) {
+			sb.append(s);
+			println();
+			if(autoFlush) {
+				flush();
+			}else if(sb.length()>BUFFER_SIZE >> 1) {
+				flushToBuffer();
+			}
+		}
+
+		public void println() {
+			sb.append(LineSeparator);
+		}
+
+		private void flushToBuffer() {
+			try {
+				os.write(sb.toString().getBytes());
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+			sb = new StringBuilder(BUFFER_SIZE);
+		}
+
+		public void flush() {
+			try {
+				flushToBuffer();
+				os.flush();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		public void close() {
+			flush();
+			try {
+				os.close();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+}
+
+
