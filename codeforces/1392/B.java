59c18a0fc0f1aaf54d7c367976572d8b546c7bef
skittles1412 <61439840+skittles1412@users.noreply.github.com>
2020-08-16 12:43:00 -0700

    Add solution for problem `B - Omkar and Infinity Clock` Link: https://codeforces.com/contest/1392/problem/B Tags: implementation, math, *800 Ref: https://codeforces.com/contest/1392/submission/90173481

---
+++ codeforces/1392/B.java
@@ -0,0 +1,186 @@
+import java.io.OutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.io.InputStreamReader;
+import java.io.BufferedOutputStream;
+import java.util.StringTokenizer;
+import java.io.Closeable;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.Flushable;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+	static class TaskAdapter implements Runnable {
+		@Override
+		public void run() {
+			InputStream inputStream = System.in;
+			OutputStream outputStream = System.out;
+			Input in = new Input(inputStream);
+			Output out = new Output(outputStream);
+			BOmkarAndInfinityClock solver = new BOmkarAndInfinityClock();
+			int testCount = Integer.parseInt(in.next());
+			for(int i = 1; i<=testCount; i++)
+				solver.solve(i, in, out);
+			out.close();
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Thread thread = new Thread(null, new TaskAdapter(), "", 1<<29);
+		thread.start();
+		thread.join();
+	}
+	static class BOmkarAndInfinityClock {
+		public BOmkarAndInfinityClock() {
+		}
+
+		public void solve(int kase, Input in, Output pw) {
+			int n = in.nextInt();
+			long k = in.nextLong();
+			long[][] arr = new long[3][n];
+			for(int i = 0; i<n; i++) {
+				arr[0][i] = in.nextInt();
+			}
+			for(int i = 0; i<2; i++) {
+				long max = Long.MIN_VALUE;
+				for(int j = 0; j<n; j++) {
+					max = Math.max(max, arr[i][j]);
+				}
+				for(int j = 0; j<n; j++) {
+					arr[i+1][j] = max-arr[i][j];
+				}
+			}
+			pw.println(arr[(int) (2-(k&1L))]);
+		}
+
+	}
+
+	static class Output implements Closeable, Flushable {
+		public StringBuilder sb;
+		public OutputStream os;
+		public int BUFFER_SIZE;
+		public boolean autoFlush;
+		public String LineSeparator;
+
+		public Output(OutputStream os) {
+			this(os, 1<<16);
+		}
+
+		public Output(OutputStream os, int bs) {
+			BUFFER_SIZE = bs;
+			sb = new StringBuilder(BUFFER_SIZE);
+			this.os = new BufferedOutputStream(os, 1<<17);
+			autoFlush = false;
+			LineSeparator = System.lineSeparator();
+		}
+
+		public void print(long l) {
+			print(String.valueOf(l));
+		}
+
+		public void print(String s) {
+			sb.append(s);
+			if(autoFlush) {
+				flush();
+			}else if(sb.length()>BUFFER_SIZE >> 1) {
+				flushToBuffer();
+			}
+		}
+
+		public void println() {
+			sb.append(LineSeparator);
+		}
+
+		public void println(long[] arr) {
+			for(int i = 0; i<arr.length; i++) {
+				if(i!=0) {
+					print(" ");
+				}
+				print(arr[i]);
+			}
+			println();
+		}
+
+		private void flushToBuffer() {
+			try {
+				os.write(sb.toString().getBytes());
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+			sb = new StringBuilder(BUFFER_SIZE);
+		}
+
+		public void flush() {
+			try {
+				flushToBuffer();
+				os.flush();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		public void close() {
+			flush();
+			try {
+				os.close();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	static class Input {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public Input(InputStream is) {
+			this(is, 1<<20);
+		}
+
+		public Input(InputStream is, int bs) {
+			br = new BufferedReader(new InputStreamReader(is), bs);
+			st = null;
+		}
+
+		public boolean hasNext() {
+			try {
+				while(st==null||!st.hasMoreTokens()) {
+					String s = br.readLine();
+					if(s==null) {
+						return false;
+					}
+					st = new StringTokenizer(s);
+				}
+				return true;
+			}catch(Exception e) {
+				return false;
+			}
+		}
+
+		public String next() {
+			if(!hasNext()) {
+				throw new InputMismatchException();
+			}
+			return st.nextToken();
+		}
+
+		public int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		public long nextLong() {
+			return Long.parseLong(next());
+		}
+
+	}
+}
+
+
