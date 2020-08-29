af16770df029294492e88e3e6747c16e52d4f27d
skittles1412 <61439840+skittles1412@users.noreply.github.com>
2020-08-28 19:54:00 -0700

    Add solution for problem `B1 - Village (Minimum)` Link: https://codeforces.com/contest/1387/problem/B1 Tags: *special problem, dp, greedy, trees, *2100 Ref: https://codeforces.com/contest/1387/submission/91252124

---
+++ codeforces/1387/B1.java
@@ -0,0 +1,377 @@
+import java.io.BufferedOutputStream;
+import java.io.Closeable;
+import java.io.DataInputStream;
+import java.io.Flushable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+	static class TaskAdapter implements Runnable {
+		@Override
+		public void run() {
+			InputStream inputStream = System.in;
+			OutputStream outputStream = System.out;
+			FastReader in = new FastReader(inputStream);
+			Output out = new Output(outputStream);
+			B1VillageMinimum solver = new B1VillageMinimum();
+			solver.solve(1, in, out);
+			out.close();
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Thread thread = new Thread(null, new TaskAdapter(), "", 1<<29);
+		thread.start();
+		thread.join();
+	}
+	static class B1VillageMinimum {
+		static B1VillageMinimum.Node[] nodes;
+		static ArrayList<Integer>[] graph;
+		static ArrayDeque<Integer> leaves;
+
+		public B1VillageMinimum() {
+		}
+
+		public void solve(int kase, InputReader in, Output pw) {
+			int n = in.nextInt();
+			graph = in.nextUndirectedGraph(n, n-1);
+			nodes = new B1VillageMinimum.Node[n];
+			leaves = new ArrayDeque<>(n);
+			for(int i = 0; i<n; i++) {
+				nodes[i] = new B1VillageMinimum.Node(i);
+			}
+			nodes[0].dfs();
+			int[] ans = new int[n];
+			long sum = 0;
+			while(!leaves.isEmpty()) {
+				B1VillageMinimum.Node cur = nodes[leaves.removeFirst()];
+				Utilities.Debug.dbg(cur.ind);
+				if(!cur.processed) {
+					if(cur.ind==0) {
+						B1VillageMinimum.Node swap = cur.children.get(0);
+						ans[swap.value] = cur.ind+1;
+						ans[cur.value] = swap.ind+1;
+						cur.processed = swap.processed = true;
+					}else {
+						B1VillageMinimum.Node swap = cur.parent;
+						ans[swap.value] = cur.ind+1;
+						ans[cur.value] = swap.ind+1;
+						swap.value ^= cur.value;
+						cur.value ^= swap.value;
+						swap.value ^= cur.value;
+						cur.processed = swap.processed = true;
+					}
+					sum += 2;
+				}
+				B1VillageMinimum.Node p = cur.parent;
+				if(p!=null) {
+					p.childcnt--;
+					if(p.childcnt==0) {
+						leaves.addLast(p.ind);
+					}
+				}
+			}
+			pw.println(sum);
+			pw.println(ans);
+		}
+
+		static class Node {
+			B1VillageMinimum.Node parent;
+			ArrayList<B1VillageMinimum.Node> children;
+			final int ind;
+			int value;
+			int childcnt;
+			boolean visited;
+			boolean processed;
+
+			public Node(int ind) {
+				this.ind = value = ind;
+				parent = null;
+				childcnt = 0;
+				children = new ArrayList<>();
+				visited = processed = false;
+			}
+
+			public void dfs() {
+				visited = true;
+				for(int i: graph[ind]) {
+					B1VillageMinimum.Node n = nodes[i];
+					if(!n.visited) {
+						n.parent = this;
+						n.dfs();
+						childcnt++;
+						children.add(n);
+					}
+				}
+				if(childcnt==0) {
+					leaves.addLast(ind);
+				}
+			}
+
+		}
+
+	}
+
+	static class Output implements Closeable, Flushable {
+		public StringBuilder sb;
+		public OutputStream os;
+		public int BUFFER_SIZE;
+		public boolean autoFlush;
+		public String LineSeparator;
+
+		public Output(OutputStream os) {
+			this(os, 1<<16);
+		}
+
+		public Output(OutputStream os, int bs) {
+			BUFFER_SIZE = bs;
+			sb = new StringBuilder(BUFFER_SIZE);
+			this.os = new BufferedOutputStream(os, 1<<17);
+			autoFlush = false;
+			LineSeparator = System.lineSeparator();
+		}
+
+		public void print(int i) {
+			print(String.valueOf(i));
+		}
+
+		public void print(String s) {
+			sb.append(s);
+			if(autoFlush) {
+				flush();
+			}else if(sb.length()>BUFFER_SIZE >> 1) {
+				flushToBuffer();
+			}
+		}
+
+		public void println(long l) {
+			println(String.valueOf(l));
+		}
+
+		public void println(String s) {
+			sb.append(s);
+			println();
+			if(autoFlush) {
+				flush();
+			}else if(sb.length()>BUFFER_SIZE >> 1) {
+				flushToBuffer();
+			}
+		}
+
+		public void println() {
+			sb.append(LineSeparator);
+		}
+
+		public void println(int[] arr) {
+			for(int i = 0; i<arr.length; i++) {
+				if(i!=0) {
+					print(" ");
+				}
+				print(arr[i]);
+			}
+			println();
+		}
+
+		private void flushToBuffer() {
+			try {
+				os.write(sb.toString().getBytes());
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+			sb = new StringBuilder(BUFFER_SIZE);
+		}
+
+		public void flush() {
+			try {
+				flushToBuffer();
+				os.flush();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		public void close() {
+			flush();
+			try {
+				os.close();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	static class Utilities {
+		public static class Debug {
+			public static final boolean LOCAL = System.getProperty("ONLINE_JUDGE")==null;
+
+			private static <T> String ts(T t) {
+				if(t==null) {
+					return "null";
+				}
+				try {
+					return ts((Iterable) t);
+				}catch(ClassCastException e) {
+					if(t instanceof int[]) {
+						String s = Arrays.toString((int[]) t);
+						return "{"+s.substring(1, s.length()-1)+"}";
+					}else if(t instanceof long[]) {
+						String s = Arrays.toString((long[]) t);
+						return "{"+s.substring(1, s.length()-1)+"}";
+					}else if(t instanceof char[]) {
+						String s = Arrays.toString((char[]) t);
+						return "{"+s.substring(1, s.length()-1)+"}";
+					}else if(t instanceof double[]) {
+						String s = Arrays.toString((double[]) t);
+						return "{"+s.substring(1, s.length()-1)+"}";
+					}else if(t instanceof boolean[]) {
+						String s = Arrays.toString((boolean[]) t);
+						return "{"+s.substring(1, s.length()-1)+"}";
+					}
+					try {
+						return ts((Object[]) t);
+					}catch(ClassCastException e1) {
+						return t.toString();
+					}
+				}
+			}
+
+			private static <T> String ts(T[] arr) {
+				StringBuilder ret = new StringBuilder();
+				ret.append("{");
+				boolean first = true;
+				for(T t: arr) {
+					if(!first) {
+						ret.append(", ");
+					}
+					first = false;
+					ret.append(ts(t));
+				}
+				ret.append("}");
+				return ret.toString();
+			}
+
+			private static <T> String ts(Iterable<T> iter) {
+				StringBuilder ret = new StringBuilder();
+				ret.append("{");
+				boolean first = true;
+				for(T t: iter) {
+					if(!first) {
+						ret.append(", ");
+					}
+					first = false;
+					ret.append(ts(t));
+				}
+				ret.append("}");
+				return ret.toString();
+			}
+
+			public static void dbg(Object... o) {
+				if(LOCAL) {
+					System.err.print("Line #"+Thread.currentThread().getStackTrace()[2].getLineNumber()+": [");
+					for(int i = 0; i<o.length; i++) {
+						if(i!=0) {
+							System.err.print(", ");
+						}
+						System.err.print(ts(o[i]));
+					}
+					System.err.println("]");
+				}
+			}
+
+		}
+
+	}
+
+	static class FastReader implements InputReader {
+		final private int BUFFER_SIZE = 1<<16;
+		private DataInputStream din;
+		private byte[] buffer;
+		private int bufferPointer;
+		private int bytesRead;
+
+		public FastReader(InputStream is) {
+			din = new DataInputStream(is);
+			buffer = new byte[BUFFER_SIZE];
+			bufferPointer = bytesRead = 0;
+		}
+
+		public int nextInt() {
+			int ret = 0;
+			byte c = skipToDigit();
+			boolean neg = (c=='-');
+			if(neg) {
+				c = read();
+			}
+			do {
+				ret = ret*10+c-'0';
+			} while((c = read())>='0'&&c<='9');
+			if(neg) {
+				return -ret;
+			}
+			return ret;
+		}
+
+		private boolean isDigit(byte b) {
+			return b>='0'&&b<='9';
+		}
+
+		private byte skipToDigit() {
+			byte ret;
+			while(!isDigit(ret = read())&&ret!='-') ;
+			return ret;
+		}
+
+		private void fillBuffer() {
+			try {
+				bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+			}catch(IOException e) {
+				e.printStackTrace();
+				throw new InputMismatchException();
+			}
+			if(bytesRead==-1) {
+				buffer[0] = -1;
+			}
+		}
+
+		private byte read() {
+			if(bytesRead==-1) {
+				throw new InputMismatchException();
+			}else if(bufferPointer==bytesRead) {
+				fillBuffer();
+			}
+			return buffer[bufferPointer++];
+		}
+
+	}
+
+	static interface InputReader {
+		int nextInt();
+
+		default ArrayList<Integer>[] nextUndirectedGraph(int n, int m) {
+			ArrayList<Integer>[] ret = new ArrayList[n];
+			for(int i = 0; i<n; i++) {
+				ret[i] = new ArrayList<>();
+			}
+			for(int i = 0; i<m; i++) {
+				int u = nextInt()-1, v = nextInt()-1;
+				ret[u].add(v);
+				ret[v].add(u);
+			}
+			return ret;
+		}
+
+	}
+}
+
+
