b617981eb87b67b985b9699ea1c4b0dcbb3ada77
skittles1412 <61439840+skittles1412@users.noreply.github.com>
2020-08-30 08:14:00 -0700

    Add solution for problem `A - Multiples of Length` Link: https://codeforces.com/contest/1396/problem/A Tags: constructive algorithms, greedy, number theory, *1600 Ref: https://codeforces.com/contest/1396/submission/91377305

---
+++ codeforces/1396/A.java
@@ -0,0 +1,256 @@
+import java.io.BufferedOutputStream;
+import java.io.Closeable;
+import java.io.DataInputStream;
+import java.io.Flushable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.InputMismatchException;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+	static class TaskAdapter implements Runnable {
+		@Override
+		public void run() {
+			InputStream inputStream = System.in;
+			OutputStream outputStream = System.out;
+			FastReader in = new FastReader(inputStream);
+			Output out = new Output(outputStream);
+			AMultiplesOfLength solver = new AMultiplesOfLength();
+			try {
+				int testNumber = 1;
+				while(true)
+					solver.solve(testNumber++, in, out);
+			}catch(InputMismatchException e) {
+				out.close();
+			}
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Thread thread = new Thread(null, new TaskAdapter(), "", 1<<29);
+		thread.start();
+		thread.join();
+	}
+	static class AMultiplesOfLength {
+		private final int iinf = 1_000_000_000;
+
+		public AMultiplesOfLength() {
+		}
+
+		public void solve(int kase, InputReader in, Output pw) {
+			int n = in.nextInt();
+			long[] arr = in.nextLong(n);
+			long add = (iinf/n+1)*n;
+			if(n==1) {
+				pw.println("1 1");
+				pw.println(-arr[0]);
+				for(int i = 0; i<2; i++) {
+					pw.println("1 1\n0");
+				}
+				return;
+			}
+			long[] a = new long[n], b = new long[n];
+			for(int i = 0; i<n; i++) {
+				b[i] = n-(arr[i]+add)%n;
+				a[i] = (arr[i]-b[i]*(n-1))/n;
+			}
+			pw.println(1, n);
+			for(int i = 0; i<n; i++) {
+				pw.print(-n*a[i]+" ");
+			}
+			pw.println();
+			pw.println(1, n-1);
+			for(int i = 0; i<n-1; i++) {
+				pw.print(-(n-1)*b[i]+" ");
+			}
+			pw.println();
+			pw.println(n, n);
+			pw.println(-arr[n-1]+a[n-1]*n);
+		}
+
+	}
+
+	static interface InputReader {
+		int nextInt();
+
+		long nextLong();
+
+		default long[] nextLong(int n) {
+			long[] ret = new long[n];
+			for(int i = 0; i<n; i++) {
+				ret[i] = nextLong();
+			}
+			return ret;
+		}
+
+	}
+
+	static class FastReader implements InputReader {
+		final private int BUFFER_SIZE = 1<<16;
+		private DataInputStream din;
+		private byte[] buffer;
+		private int bufferPointer;
+		private int bytesRead;
+
+		public FastReader(InputStream is) {
+			din = new DataInputStream(is);
+			buffer = new byte[BUFFER_SIZE];
+			bufferPointer = bytesRead = 0;
+		}
+
+		public int nextInt() {
+			int ret = 0;
+			byte c = skipToDigit();
+			boolean neg = (c=='-');
+			if(neg) {
+				c = read();
+			}
+			do {
+				ret = ret*10+c-'0';
+			} while((c = read())>='0'&&c<='9');
+			if(neg) {
+				return -ret;
+			}
+			return ret;
+		}
+
+		public long nextLong() {
+			long ret = 0;
+			byte c = skipToDigit();
+			boolean neg = (c=='-');
+			if(neg) {
+				c = read();
+			}
+			do {
+				ret = ret*10+c-'0';
+			} while((c = read())>='0'&&c<='9');
+			if(neg) {
+				return -ret;
+			}
+			return ret;
+		}
+
+		private boolean isDigit(byte b) {
+			return b>='0'&&b<='9';
+		}
+
+		private byte skipToDigit() {
+			byte ret;
+			while(!isDigit(ret = read())&&ret!='-') ;
+			return ret;
+		}
+
+		private void fillBuffer() {
+			try {
+				bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+			}catch(IOException e) {
+				e.printStackTrace();
+				throw new InputMismatchException();
+			}
+			if(bytesRead==-1) {
+				buffer[0] = -1;
+			}
+		}
+
+		private byte read() {
+			if(bytesRead==-1) {
+				throw new InputMismatchException();
+			}else if(bufferPointer==bytesRead) {
+				fillBuffer();
+			}
+			return buffer[bufferPointer++];
+		}
+
+	}
+
+	static class Output implements Closeable, Flushable {
+		public StringBuilder sb;
+		public OutputStream os;
+		public int BUFFER_SIZE;
+		public boolean autoFlush;
+		public String LineSeparator;
+
+		public Output(OutputStream os) {
+			this(os, 1<<16);
+		}
+
+		public Output(OutputStream os, int bs) {
+			BUFFER_SIZE = bs;
+			sb = new StringBuilder(BUFFER_SIZE);
+			this.os = new BufferedOutputStream(os, 1<<17);
+			autoFlush = false;
+			LineSeparator = System.lineSeparator();
+		}
+
+		public void print(String s) {
+			sb.append(s);
+			if(autoFlush) {
+				flush();
+			}else if(sb.length()>BUFFER_SIZE >> 1) {
+				flushToBuffer();
+			}
+		}
+
+		public void println(long l) {
+			println(String.valueOf(l));
+		}
+
+		public void println(Object... o) {
+			for(int i = 0; i<o.length; i++) {
+				if(i!=0) {
+					print(" ");
+				}
+				print(String.valueOf(o[i]));
+			}
+			println();
+		}
+
+		public void println(String s) {
+			sb.append(s);
+			println();
+			if(autoFlush) {
+				flush();
+			}else if(sb.length()>BUFFER_SIZE >> 1) {
+				flushToBuffer();
+			}
+		}
+
+		public void println() {
+			sb.append(LineSeparator);
+		}
+
+		private void flushToBuffer() {
+			try {
+				os.write(sb.toString().getBytes());
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+			sb = new StringBuilder(BUFFER_SIZE);
+		}
+
+		public void flush() {
+			try {
+				flushToBuffer();
+				os.flush();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		public void close() {
+			flush();
+			try {
+				os.close();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+}
+
+
