59b0bc620cd7284ab8cf12cccc1a8f242b18fb63
skittles1412 <61439840+skittles1412@users.noreply.github.com>
2020-09-10 13:49:00 -0700

    Add solution for problem `D - Again?` Link: https://codeforces.com/contest/1331/problem/D Tags: implementation Ref: https://codeforces.com/contest/1331/submission/92458788

---
+++ codeforces/1331/D.java
@@ -0,0 +1,187 @@
+import java.io.BufferedOutputStream;
+import java.io.Closeable;
+import java.io.DataInputStream;
+import java.io.Flushable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.InputMismatchException;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+	static class TaskAdapter implements Runnable {
+		@Override
+		public void run() {
+			InputStream inputStream = System.in;
+			OutputStream outputStream = System.out;
+			FastReader in = new FastReader(inputStream);
+			Output out = new Output(outputStream);
+			DAgain solver = new DAgain();
+			solver.solve(1, in, out);
+			out.close();
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Thread thread = new Thread(null, new TaskAdapter(), "", 1<<28);
+		thread.start();
+		thread.join();
+	}
+	static class DAgain {
+		public DAgain() {
+		}
+
+		public void solve(int kase, InputReader in, Output pw) {
+			String s = in.next(7);
+			pw.println((s.charAt(6)-'0')&1);
+		}
+
+	}
+
+	static class FastReader implements InputReader {
+		final private int BUFFER_SIZE = 1<<16;
+		private DataInputStream din;
+		private byte[] buffer;
+		private int bufferPointer;
+		private int bytesRead;
+
+		public FastReader(InputStream is) {
+			din = new DataInputStream(is);
+			buffer = new byte[BUFFER_SIZE];
+			bufferPointer = bytesRead = 0;
+		}
+
+		public String next() {
+			StringBuilder ret = new StringBuilder(64);
+			byte c = skip();
+			while(c!=-1&&!isSpaceChar(c)) {
+				ret.appendCodePoint(c);
+				c = read();
+			}
+			return ret.toString();
+		}
+
+		public String next(int maxLength) {
+			byte[] ret = new byte[maxLength];
+			byte c = skip();
+			int ind = 0;
+			while(c!=-1&&!isSpaceChar(c)) {
+				ret[ind++] = c;
+				c = read();
+			}
+			return new String(ret, 0, ind);
+		}
+
+		private boolean isSpaceChar(byte b) {
+			return b==' '||b=='\r'||b=='\n'||b=='\t'||b=='\f';
+		}
+
+		private byte skip() {
+			byte ret;
+			while(isSpaceChar((ret = read()))) ;
+			return ret;
+		}
+
+		private void fillBuffer() {
+			try {
+				bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+			}catch(IOException e) {
+				e.printStackTrace();
+				throw new InputMismatchException();
+			}
+			if(bytesRead==-1) {
+				buffer[0] = -1;
+			}
+		}
+
+		private byte read() {
+			if(bytesRead==-1) {
+				throw new InputMismatchException();
+			}else if(bufferPointer==bytesRead) {
+				fillBuffer();
+			}
+			return buffer[bufferPointer++];
+		}
+
+	}
+
+	static class Output implements Closeable, Flushable {
+		public StringBuilder sb;
+		public OutputStream os;
+		public int BUFFER_SIZE;
+		public boolean autoFlush;
+		public String lineSeparator;
+
+		public Output(OutputStream os) {
+			this(os, 1<<16);
+		}
+
+		public Output(OutputStream os, int bs) {
+			BUFFER_SIZE = bs;
+			sb = new StringBuilder(BUFFER_SIZE);
+			this.os = new BufferedOutputStream(os, 1<<17);
+			autoFlush = false;
+			lineSeparator = System.lineSeparator();
+		}
+
+		public void println(int i) {
+			println(String.valueOf(i));
+		}
+
+		public void println(String s) {
+			sb.append(s);
+			println();
+			if(autoFlush) {
+				flush();
+			}else if(sb.length()>BUFFER_SIZE >> 1) {
+				flushToBuffer();
+			}
+		}
+
+		public void println() {
+			sb.append(lineSeparator);
+		}
+
+		private void flushToBuffer() {
+			try {
+				os.write(sb.toString().getBytes());
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+			sb = new StringBuilder(BUFFER_SIZE);
+		}
+
+		public void flush() {
+			try {
+				flushToBuffer();
+				os.flush();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		public void close() {
+			flush();
+			try {
+				os.close();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	static interface InputReader {
+		String next();
+
+		default String next(int maxLength) {
+			return next();
+		}
+
+	}
+}
+
+
