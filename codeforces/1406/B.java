c96eace2ce290bbcdfe8d559014a4917e1a92e6b
skittles1412 <61439840+skittles1412@users.noreply.github.com>
2020-09-12 06:57:00 -0700

    Add solution for problem `B - Maximum Product` Link: https://codeforces.com/contest/1406/problem/B Tags: brute force, dp, greedy, implementation, sortings, *1200 Ref: https://codeforces.com/contest/1406/submission/92588892

---
+++ codeforces/1406/B.java
@@ -0,0 +1,243 @@
+import java.io.BufferedOutputStream;
+import java.io.Closeable;
+import java.io.DataInputStream;
+import java.io.Flushable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.InputMismatchException;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+	static class TaskAdapter implements Runnable {
+		@Override
+		public void run() {
+			long startTime = System.currentTimeMillis();
+			InputStream inputStream = System.in;
+			OutputStream outputStream = System.out;
+			FastReader in = new FastReader(inputStream);
+			Output out = new Output(outputStream);
+			BMaximumProduct solver = new BMaximumProduct();
+			int testCount = Integer.parseInt(in.next());
+			for(int i = 1; i<=testCount; i++)
+				solver.solve(i, in, out);
+			out.close();
+			System.err.println(System.currentTimeMillis()-startTime+"ms");
+		}
+
+	}
+
+	public static void main(String[] args) throws Exception {
+		Thread thread = new Thread(null, new TaskAdapter(), "", 1<<28);
+		thread.start();
+		thread.join();
+	}
+
+	static class BMaximumProduct {
+		private final long linf = 4_000_000_000_000_000_000L;
+		int n;
+
+		public BMaximumProduct() {
+		}
+
+		public long solve(ArrayList<Long> neg, ArrayList<Long> pos, int i) {
+			long ne = 1, p = 1;
+			for(int j = 0; j<i; j++) {
+				ne *= neg.get(j);
+			}
+			for(int j = 0; j<5-i; j++) {
+				p *= pos.get(j);
+			}
+			return ne*p;
+		}
+
+		public void solve(int kase, InputReader in, Output pw) {
+			ArrayList<Long> neg = new ArrayList<>(), pos = new ArrayList<>();
+			long ans = -linf;
+			n = in.nextInt();
+			for(int i = 0; i<n; i++) {
+				long x = in.nextInt();
+				if(x<0) {
+					neg.add(x);
+				}else if(x>0) {
+					pos.add(x);
+				}else {
+					ans = 0;
+				}
+			}
+			neg.sort(null);
+			pos.sort(Comparator.comparingLong(o -> -o));
+			for(int i = 0; i<=5; i++) {
+				if(neg.size()<i||pos.size()<5-i) {
+					continue;
+				}
+				ans = Math.max(ans, solve(neg, pos, i));
+				Collections.reverse(neg);
+				ans = Math.max(ans, solve(neg, pos, i));
+				Collections.reverse(pos);
+				ans = Math.max(ans, solve(neg, pos, i));
+				Collections.reverse(neg);
+				ans = Math.max(ans, solve(neg, pos, i));
+				Collections.reverse(pos);
+			}
+			pw.println(ans);
+		}
+
+	}
+
+	static class FastReader implements InputReader {
+		final private int BUFFER_SIZE = 1<<16;
+		private DataInputStream din;
+		private byte[] buffer;
+		private int bufferPointer;
+		private int bytesRead;
+
+		public FastReader(InputStream is) {
+			din = new DataInputStream(is);
+			buffer = new byte[BUFFER_SIZE];
+			bufferPointer = bytesRead = 0;
+		}
+
+		public String next() {
+			StringBuilder ret = new StringBuilder(64);
+			byte c = skip();
+			while(c!=-1&&!isSpaceChar(c)) {
+				ret.appendCodePoint(c);
+				c = read();
+			}
+			return ret.toString();
+		}
+
+		public int nextInt() {
+			int ret = 0;
+			byte c = skipToDigit();
+			boolean neg = (c=='-');
+			if(neg) {
+				c = read();
+			}
+			do {
+				ret = ret*10+c-'0';
+			} while((c = read())>='0'&&c<='9');
+			if(neg) {
+				return -ret;
+			}
+			return ret;
+		}
+
+		private boolean isSpaceChar(byte b) {
+			return b==' '||b=='\r'||b=='\n'||b=='\t'||b=='\f';
+		}
+
+		private byte skip() {
+			byte ret;
+			while(isSpaceChar((ret = read()))) ;
+			return ret;
+		}
+
+		private boolean isDigit(byte b) {
+			return b>='0'&&b<='9';
+		}
+
+		private byte skipToDigit() {
+			byte ret;
+			while(!isDigit(ret = read())&&ret!='-') ;
+			return ret;
+		}
+
+		private void fillBuffer() {
+			try {
+				bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+			}catch(IOException e) {
+				e.printStackTrace();
+				throw new InputMismatchException();
+			}
+			if(bytesRead==-1) {
+				buffer[0] = -1;
+			}
+		}
+
+		private byte read() {
+			if(bytesRead==-1) {
+				throw new InputMismatchException();
+			}else if(bufferPointer==bytesRead) {
+				fillBuffer();
+			}
+			return buffer[bufferPointer++];
+		}
+
+	}
+
+	static class Output implements Closeable, Flushable {
+		public StringBuilder sb;
+		public OutputStream os;
+		public int BUFFER_SIZE;
+		public String lineSeparator;
+
+		public Output(OutputStream os) {
+			this(os, 1<<16);
+		}
+
+		public Output(OutputStream os, int bs) {
+			BUFFER_SIZE = bs;
+			sb = new StringBuilder(BUFFER_SIZE);
+			this.os = new BufferedOutputStream(os, 1<<17);
+			lineSeparator = System.lineSeparator();
+		}
+
+		public void println(long l) {
+			println(String.valueOf(l));
+		}
+
+		public void println(String s) {
+			sb.append(s);
+			println();
+			if(sb.length()>BUFFER_SIZE >> 1) {
+				flushToBuffer();
+			}
+		}
+
+		public void println() {
+			sb.append(lineSeparator);
+		}
+
+		private void flushToBuffer() {
+			try {
+				os.write(sb.toString().getBytes());
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+			sb = new StringBuilder(BUFFER_SIZE);
+		}
+
+		public void flush() {
+			try {
+				flushToBuffer();
+				os.flush();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		public void close() {
+			flush();
+			try {
+				os.close();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	static interface InputReader {
+		int nextInt();
+
+	}
+}
+
