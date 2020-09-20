233f9b59da176de10f17d021656df06bab70cccd
skittles1412 <61439840+skittles1412@users.noreply.github.com>
2020-09-20 13:44:00 -0700

    Add solution for problem `G - Candy Box (hard version)` Link: https://codeforces.com/contest/1183/problem/G Tags: greedy, implementation, sortings, *2000 Ref: https://codeforces.com/contest/1183/submission/93366132

---
+++ codeforces/1183/G.java
@@ -0,0 +1,285 @@
+import java.io.BufferedOutputStream;
+import java.io.Closeable;
+import java.io.DataInputStream;
+import java.io.Flushable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.AbstractCollection;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.InputMismatchException;
+import java.util.Objects;
+import java.util.PriorityQueue;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+	static class TaskAdapter implements Runnable {
+		@Override
+		public void run() {
+			long startTime = System.currentTimeMillis();
+			InputStream inputStream = System.in;
+			OutputStream outputStream = System.out;
+			FastReader in = new FastReader(inputStream);
+			Output out = new Output(outputStream);
+			GCandyBoxHardVersion solver = new GCandyBoxHardVersion();
+			int testCount = Integer.parseInt(in.next());
+			for(int i = 1; i<=testCount; i++)
+				solver.solve(i, in, out);
+			out.close();
+			System.err.println(System.currentTimeMillis()-startTime+"ms");
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Thread thread = new Thread(null, new TaskAdapter(), "", 1<<28);
+		thread.start();
+		thread.join();
+	}
+
+	static class GCandyBoxHardVersion {
+		public GCandyBoxHardVersion() {
+		}
+
+		public void solve(int kase, InputReader in, Output pw) {
+			int n = in.nextInt();
+			Pair<Integer, Integer>[] arr = new Pair[n];
+			for(int i = 0; i<n; i++) {
+				arr[i] = new Pair<>(0, 0);
+			}
+			for(int i = 0; i<n; i++) {
+				int x = in.nextInt()-1;
+				arr[x].a++;
+				arr[x].b += in.nextInt();
+			}
+			Arrays.sort(arr);
+			Utilities.reverse(arr, 0, n-1);
+			int a = 0, b = 0, j = 0;
+			PriorityQueue<Integer> pq = new PriorityQueue<>(n, Comparator.comparingInt(o -> -o));
+			for(int i = n; i>=0; i--) {
+				while(j<n&&arr[j].a>=i) {
+					pq.add(arr[j++].b);
+				}
+				if(!pq.isEmpty()) {
+					a += i;
+					b += Math.min(i, pq.poll());
+				}
+			}
+			pw.println(a, b);
+		}
+
+	}
+
+	static class Output implements Closeable, Flushable {
+		public StringBuilder sb;
+		public OutputStream os;
+		public int BUFFER_SIZE;
+		public String lineSeparator;
+
+		public Output(OutputStream os) {
+			this(os, 1<<16);
+		}
+
+		public Output(OutputStream os, int bs) {
+			BUFFER_SIZE = bs;
+			sb = new StringBuilder(BUFFER_SIZE);
+			this.os = new BufferedOutputStream(os, 1<<17);
+			lineSeparator = System.lineSeparator();
+		}
+
+		public void print(String s) {
+			sb.append(s);
+			if(sb.length()>BUFFER_SIZE >> 1) {
+				flushToBuffer();
+			}
+		}
+
+		public void println(Object... o) {
+			for(int i = 0; i<o.length; i++) {
+				if(i!=0) {
+					print(" ");
+				}
+				print(String.valueOf(o[i]));
+			}
+			println();
+		}
+
+		public void println() {
+			sb.append(lineSeparator);
+		}
+
+		private void flushToBuffer() {
+			try {
+				os.write(sb.toString().getBytes());
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+			sb = new StringBuilder(BUFFER_SIZE);
+		}
+
+		public void flush() {
+			try {
+				flushToBuffer();
+				os.flush();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		public void close() {
+			flush();
+			try {
+				os.close();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	static class Pair<T1, T2> implements Comparable<Pair<T1, T2>> {
+		public T1 a;
+		public T2 b;
+
+		public Pair(Pair<T1, T2> p) {
+			this(p.a, p.b);
+		}
+
+		public Pair(T1 a, T2 b) {
+			this.a = a;
+			this.b = b;
+		}
+
+		public String toString() {
+			return a+" "+b;
+		}
+
+		public int hashCode() {
+			return Objects.hash(a, b);
+		}
+
+		public boolean equals(Object o) {
+			if(o instanceof Pair) {
+				Pair p = (Pair) o;
+				return a.equals(p.a)&&b.equals(p.b);
+			}
+			return false;
+		}
+
+		public int compareTo(Pair<T1, T2> p) {
+			int cmp = ((Comparable<T1>) a).compareTo(p.a);
+			if(cmp==0) {
+				return ((Comparable<T2>) b).compareTo(p.b);
+			}
+			return cmp;
+		}
+
+	}
+
+	static class FastReader implements InputReader {
+		final private int BUFFER_SIZE = 1<<16;
+		private DataInputStream din;
+		private byte[] buffer;
+		private int bufferPointer;
+		private int bytesRead;
+
+		public FastReader(InputStream is) {
+			din = new DataInputStream(is);
+			buffer = new byte[BUFFER_SIZE];
+			bufferPointer = bytesRead = 0;
+		}
+
+		public String next() {
+			StringBuilder ret = new StringBuilder(64);
+			byte c = skip();
+			while(c!=-1&&!isSpaceChar(c)) {
+				ret.appendCodePoint(c);
+				c = read();
+			}
+			return ret.toString();
+		}
+
+		public int nextInt() {
+			int ret = 0;
+			byte c = skipToDigit();
+			boolean neg = (c=='-');
+			if(neg) {
+				c = read();
+			}
+			do {
+				ret = ret*10+c-'0';
+			} while((c = read())>='0'&&c<='9');
+			if(neg) {
+				return -ret;
+			}
+			return ret;
+		}
+
+		private boolean isSpaceChar(byte b) {
+			return b==' '||b=='\r'||b=='\n'||b=='\t'||b=='\f';
+		}
+
+		private byte skip() {
+			byte ret;
+			while(isSpaceChar((ret = read()))) ;
+			return ret;
+		}
+
+		private boolean isDigit(byte b) {
+			return b>='0'&&b<='9';
+		}
+
+		private byte skipToDigit() {
+			byte ret;
+			while(!isDigit(ret = read())&&ret!='-') ;
+			return ret;
+		}
+
+		private void fillBuffer() {
+			try {
+				bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+			}catch(IOException e) {
+				e.printStackTrace();
+				throw new InputMismatchException();
+			}
+			if(bytesRead==-1) {
+				buffer[0] = -1;
+			}
+		}
+
+		private byte read() {
+			if(bytesRead==-1) {
+				throw new InputMismatchException();
+			}else if(bufferPointer==bytesRead) {
+				fillBuffer();
+			}
+			return buffer[bufferPointer++];
+		}
+
+	}
+
+	static interface InputReader {
+		int nextInt();
+
+	}
+
+	static class Utilities {
+		public static void swap(Object[] arr, int i, int j) {
+			Object o = arr[i];
+			arr[i] = arr[j];
+			arr[j] = o;
+		}
+
+		public static void reverse(Object[] arr, int i, int j) {
+			while(i<j) {
+				swap(arr, i++, j--);
+			}
+		}
+
+	}
+}
+
+
