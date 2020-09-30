23abb21c9be00286732d80c7612d73ca99158d7f
skittles1412 <61439840+skittles1412@users.noreply.github.com>
2020-09-29 20:49:00 -0700

    Add solution for problem `F - Yet Another Substring Reverse` Link: https://codeforces.com/contest/1234/problem/F Tags: bitmasks, dp, *2200 Ref: https://codeforces.com/contest/1234/submission/94258027

---
+++ codeforces/1234/F.java
@@ -0,0 +1,199 @@
+import java.io.BufferedOutputStream;
+import java.io.Closeable;
+import java.io.DataInputStream;
+import java.io.Flushable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.InputMismatchException;
+import java.util.function.Function;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+	static class TaskAdapter implements Runnable {
+		@Override
+		public void run() {
+			long startTime = System.currentTimeMillis();
+			InputStream inputStream = System.in;
+			OutputStream outputStream = System.out;
+			FastReader in = new FastReader(inputStream);
+			Output out = new Output(outputStream);
+			FYetAnotherSubstringReverse solver = new FYetAnotherSubstringReverse();
+			solver.solve(1, in, out);
+			out.close();
+			System.err.println(System.currentTimeMillis()-startTime+"ms");
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Thread thread = new Thread(null, new TaskAdapter(), "", 1<<28);
+		thread.start();
+		thread.join();
+	}
+
+	static class FYetAnotherSubstringReverse {
+		public FYetAnotherSubstringReverse() {
+		}
+
+		public void solve(int kase, InputReader in, Output pw) {
+			int[] arr = in.nextIntChar(o -> o-'a');
+			int n = arr.length, m = 20;
+			int[] dp = new int[1<<m];
+			for(int i = 0; i<n; i++) {
+				int mask = 0;
+				for(int j = i; j<i+m&&j<n; j++) {
+					if((mask&1<<arr[j])>0) {
+						break;
+					}
+					dp[mask |= 1<<arr[j]] = j-i+1;
+				}
+			}
+			for(int i = 1; i<1<<m; i++) {
+				for(int j = 0; j<m; j++) {
+					if((i&1<<j)>0) {
+						dp[i] = Math.max(dp[i], dp[i^(1<<j)]);
+					}
+				}
+			}
+			int ans = 0;
+			for(int i = 0; i<1<<m; i++) {
+				ans = Math.max(ans, dp[i]+dp[((1<<m)-1)^i]);
+			}
+			pw.println(ans);
+		}
+
+	}
+
+	static interface InputReader {
+		String next();
+
+		default int[] nextIntChar(Function<Character, Integer> f) {
+			String s = next();
+			int[] ret = new int[s.length()];
+			for(int i = 0; i<s.length(); i++) {
+				ret[i] = f.apply(s.charAt(i));
+			}
+			return ret;
+		}
+
+	}
+
+	static class FastReader implements InputReader {
+		final private int BUFFER_SIZE = 1<<16;
+		private DataInputStream din;
+		private byte[] buffer;
+		private int bufferPointer;
+		private int bytesRead;
+
+		public FastReader(InputStream is) {
+			din = new DataInputStream(is);
+			buffer = new byte[BUFFER_SIZE];
+			bufferPointer = bytesRead = 0;
+		}
+
+		public String next() {
+			StringBuilder ret = new StringBuilder(64);
+			byte c = skip();
+			while(c!=-1&&!isSpaceChar(c)) {
+				ret.appendCodePoint(c);
+				c = read();
+			}
+			return ret.toString();
+		}
+
+		private boolean isSpaceChar(byte b) {
+			return b==' '||b=='\r'||b=='\n'||b=='\t'||b=='\f';
+		}
+
+		private byte skip() {
+			byte ret;
+			while(isSpaceChar((ret = read()))) ;
+			return ret;
+		}
+
+		private void fillBuffer() {
+			try {
+				bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+			}catch(IOException e) {
+				e.printStackTrace();
+				throw new InputMismatchException();
+			}
+			if(bytesRead==-1) {
+				buffer[0] = -1;
+			}
+		}
+
+		private byte read() {
+			if(bytesRead==-1) {
+				throw new InputMismatchException();
+			}else if(bufferPointer==bytesRead) {
+				fillBuffer();
+			}
+			return buffer[bufferPointer++];
+		}
+
+	}
+
+	static class Output implements Closeable, Flushable {
+		public StringBuilder sb;
+		public OutputStream os;
+		public int BUFFER_SIZE;
+		public String lineSeparator;
+
+		public Output(OutputStream os) {
+			this(os, 1<<16);
+		}
+
+		public Output(OutputStream os, int bs) {
+			BUFFER_SIZE = bs;
+			sb = new StringBuilder(BUFFER_SIZE);
+			this.os = new BufferedOutputStream(os, 1<<17);
+			lineSeparator = System.lineSeparator();
+		}
+
+		public void println(int i) {
+			println(String.valueOf(i));
+		}
+
+		public void println(String s) {
+			sb.append(s);
+			println();
+		}
+
+		public void println() {
+			sb.append(lineSeparator);
+		}
+
+		private void flushToBuffer() {
+			try {
+				os.write(sb.toString().getBytes());
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+			sb = new StringBuilder(BUFFER_SIZE);
+		}
+
+		public void flush() {
+			try {
+				flushToBuffer();
+				os.flush();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		public void close() {
+			flush();
+			try {
+				os.close();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+}
+
