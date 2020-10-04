e5a1db21b3b21bf11269e365a979f779c308e026
skittles1412 <61439840+skittles1412@users.noreply.github.com>
2020-10-04 08:28:00 -0700

    Add solution for problem `F - Maximum White Subtree` Link: https://codeforces.com/contest/1324/problem/F Tags: dfs and similar, dp, graphs, trees, *1800 Ref: https://codeforces.com/contest/1324/submission/94658709

---
+++ codeforces/1324/F.java
@@ -0,0 +1,331 @@
+import java.io.BufferedOutputStream;
+import java.io.Closeable;
+import java.io.DataInputStream;
+import java.io.Flushable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.function.IntUnaryOperator;
+import java.util.stream.Stream;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+	static class TaskAdapter implements Runnable {
+		@Override
+		public void run() {
+			long startTime = System.currentTimeMillis();
+			InputStream inputStream = System.in;
+			OutputStream outputStream = System.out;
+			FastReader in = new FastReader(inputStream);
+			Output out = new Output(outputStream);
+			FMaximumWhiteSubtree solver = new FMaximumWhiteSubtree();
+			solver.solve(1, in, out);
+			out.close();
+			System.err.println(System.currentTimeMillis()-startTime+"ms");
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Thread thread = new Thread(null, new TaskAdapter(), "", 1<<28);
+		thread.start();
+		thread.join();
+	}
+
+	static class FMaximumWhiteSubtree {
+		int n;
+		int[] arr;
+		int[] dp;
+		int[] ans;
+		ArrayList<Integer>[] graph;
+
+		public FMaximumWhiteSubtree() {
+		}
+
+		public int dp(int u) {
+			int ans = arr[u];
+			for(int v: graph[u]) {
+				graph[v].remove((Integer) u);
+				int sum = dp(v);
+				if(sum>=0) {
+					ans += sum;
+				}
+			}
+			return dp[u] = ans;
+		}
+
+		public void dfs(int u) {
+			ans[u] = dp[u];
+			for(int v: graph[u]) {
+				dp[u] -= Math.max(0, dp[v]);
+				dp[v] += Math.max(0, dp[u]);
+				dfs(v);
+				dp[v] -= Math.max(0, dp[u]);
+				dp[u] += Math.max(0, dp[v]);
+			}
+		}
+
+		public void solve(int kase, InputReader in, Output pw) {
+			n = in.nextInt();
+			arr = in.nextInt(n, o -> o==1 ? 1 : -1);
+			graph = in.nextUndirectedGraph(n, n-1);
+			dp = new int[n];
+			dp(0);
+			ans = new int[n];
+////			Utilities.Debug.dbg(dp);
+			dfs(0);
+			pw.println(ans);
+		}
+
+	}
+
+	static class FastReader implements InputReader {
+		final private int BUFFER_SIZE = 1<<16;
+		private DataInputStream din;
+		private byte[] buffer;
+		private int bufferPointer;
+		private int bytesRead;
+
+		public FastReader(InputStream is) {
+			din = new DataInputStream(is);
+			buffer = new byte[BUFFER_SIZE];
+			bufferPointer = bytesRead = 0;
+		}
+
+		public int nextInt() {
+			int ret = 0;
+			byte c = skipToDigit();
+			boolean neg = (c=='-');
+			if(neg) {
+				c = read();
+			}
+			do {
+				ret = ret*10+c-'0';
+			} while((c = read())>='0'&&c<='9');
+			if(neg) {
+				return -ret;
+			}
+			return ret;
+		}
+
+		private boolean isDigit(byte b) {
+			return b>='0'&&b<='9';
+		}
+
+		private byte skipToDigit() {
+			byte ret;
+			while(!isDigit(ret = read())&&ret!='-') ;
+			return ret;
+		}
+
+		private void fillBuffer() {
+			try {
+				bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+			}catch(IOException e) {
+				e.printStackTrace();
+				throw new InputMismatchException();
+			}
+			if(bytesRead==-1) {
+				buffer[0] = -1;
+			}
+		}
+
+		private byte read() {
+			if(bytesRead==-1) {
+				throw new InputMismatchException();
+			}else if(bufferPointer==bytesRead) {
+				fillBuffer();
+			}
+			return buffer[bufferPointer++];
+		}
+
+	}
+
+	static class Output implements Closeable, Flushable {
+		public StringBuilder sb;
+		public OutputStream os;
+		public int BUFFER_SIZE;
+		public String lineSeparator;
+
+		public Output(OutputStream os) {
+			this(os, 1<<16);
+		}
+
+		public Output(OutputStream os, int bs) {
+			BUFFER_SIZE = bs;
+			sb = new StringBuilder(BUFFER_SIZE);
+			this.os = new BufferedOutputStream(os, 1<<17);
+			lineSeparator = System.lineSeparator();
+		}
+
+		public void print(int i) {
+			print(String.valueOf(i));
+		}
+
+		public void print(String s) {
+			sb.append(s);
+			if(sb.length()>BUFFER_SIZE >> 1) {
+				flushToBuffer();
+			}
+		}
+
+		public void println() {
+			sb.append(lineSeparator);
+		}
+
+		public void println(int[] arr) {
+			for(int i = 0; i<arr.length; i++) {
+				if(i!=0) {
+					print(" ");
+				}
+				print(arr[i]);
+			}
+			println();
+		}
+
+		private void flushToBuffer() {
+			try {
+				os.write(sb.toString().getBytes());
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+			sb = new StringBuilder(BUFFER_SIZE);
+		}
+
+		public void flush() {
+			try {
+				flushToBuffer();
+				os.flush();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		public void close() {
+			flush();
+			try {
+				os.close();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	static interface InputReader {
+		int nextInt();
+
+		default int[] nextInt(int n, IntUnaryOperator operator) {
+			int[] ret = new int[n];
+			for(int i = 0; i<n; i++) {
+				ret[i] = operator.applyAsInt(nextInt());
+			}
+			return ret;
+		}
+
+		default ArrayList<Integer>[] nextUndirectedGraph(int n, int m) {
+//			ArrayList<Integer>[] ret = new ArrayList[n];
+//			for(int i = 0; i<n; i++) {
+//				ret[i] = new ArrayList<>();
+//			}
+			ArrayList<Integer>[] ret = Stream.generate(ArrayList::new).limit(n).toArray(ArrayList[]::new);
+			for(int i = 0; i<m; i++) {
+				int u = nextInt()-1, v = nextInt()-1;
+				ret[u].add(v);
+				ret[v].add(u);
+			}
+			return ret;
+		}
+
+	}
+
+	static class Utilities {
+		public static class Debug {
+			public static final boolean LOCAL = System.getProperty("ONLINE_JUDGE")==null;
+
+			private static <T> String ts(T t) {
+				if(t==null) {
+					return "null";
+				}
+				try {
+					return ts((Iterable) t);
+				}catch(ClassCastException e) {
+					if(t instanceof int[]) {
+						String s = Arrays.toString((int[]) t);
+						return "{"+s.substring(1, s.length()-1)+"}";
+					}else if(t instanceof long[]) {
+						String s = Arrays.toString((long[]) t);
+						return "{"+s.substring(1, s.length()-1)+"}";
+					}else if(t instanceof char[]) {
+						String s = Arrays.toString((char[]) t);
+						return "{"+s.substring(1, s.length()-1)+"}";
+					}else if(t instanceof double[]) {
+						String s = Arrays.toString((double[]) t);
+						return "{"+s.substring(1, s.length()-1)+"}";
+					}else if(t instanceof boolean[]) {
+						String s = Arrays.toString((boolean[]) t);
+						return "{"+s.substring(1, s.length()-1)+"}";
+					}
+					try {
+						return ts((Object[]) t);
+					}catch(ClassCastException e1) {
+						return t.toString();
+					}
+				}
+			}
+
+			private static <T> String ts(T[] arr) {
+				StringBuilder ret = new StringBuilder();
+				ret.append("{");
+				boolean first = true;
+				for(T t: arr) {
+					if(!first) {
+						ret.append(", ");
+					}
+					first = false;
+					ret.append(ts(t));
+				}
+				ret.append("}");
+				return ret.toString();
+			}
+
+			private static <T> String ts(Iterable<T> iter) {
+				StringBuilder ret = new StringBuilder();
+				ret.append("{");
+				boolean first = true;
+				for(T t: iter) {
+					if(!first) {
+						ret.append(", ");
+					}
+					first = false;
+					ret.append(ts(t));
+				}
+				ret.append("}");
+				return ret.toString();
+			}
+
+			public static void dbg(Object... o) {
+				if(LOCAL) {
+					System.err.print("Line #"+Thread.currentThread().getStackTrace()[2].getLineNumber()+": [");
+					for(int i = 0; i<o.length; i++) {
+						if(i!=0) {
+							System.err.print(", ");
+						}
+						System.err.print(ts(o[i]));
+					}
+					System.err.println("]");
+				}
+			}
+
+		}
+
+	}
+}
+
+
