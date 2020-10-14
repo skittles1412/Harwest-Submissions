31babedadd734e3f511fddbca3a0a4dacf549003
skittles1412 <61439840+skittles1412@users.noreply.github.com>
2020-10-14 16:51:00 -0700

    Add solution for problem `B - Subway Pursuit` Link: https://codeforces.com/contest/1039/problem/B Tags: binary search, interactive, probabilities, *2100 Ref: https://codeforces.com/contest/1039/submission/95546311

---
+++ codeforces/1039/B.java
@@ -0,0 +1,313 @@
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.Closeable;
+import java.io.Flushable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.Random;
+import java.util.StringTokenizer;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+	static class TaskAdapter implements Runnable {
+		@Override
+		public void run() {
+			long startTime = System.currentTimeMillis();
+			InputStream inputStream = System.in;
+			OutputStream outputStream = System.out;
+			Input in = new Input(inputStream);
+			Output out = new Output(outputStream);
+			BSubwayPursuit solver = new BSubwayPursuit();
+			solver.solve(1, in, out);
+			out.close();
+			System.err.println(System.currentTimeMillis()-startTime+"ms");
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Thread thread = new Thread(null, new TaskAdapter(), "", 1<<28);
+		thread.start();
+		thread.join();
+	}
+
+	static class BSubwayPursuit {
+		Random rand = new Random();
+		InputReader in;
+		Output pw;
+		long n;
+		long k;
+		long l;
+		long r;
+
+		public BSubwayPursuit() {
+		}
+
+		public boolean bsearch() {
+			boolean equal = false;
+			while((r-l)>k<<2) {
+				long mid = r+l >> 1;
+				pw.println(l, mid);
+				pw.flush();
+				boolean valid = in.next().charAt(0)=='Y';
+				if(valid) {
+					r = mid;
+				}else {
+					l = mid+1;
+				}
+				if(l==mid&&valid) {
+					equal = true;
+				}
+				updateRange();
+			}
+			return equal;
+		}
+
+		public void updateRange() {
+			r += k;
+			l -= k;
+			r = Math.min(r, n);
+			l = Math.max(l, 1);
+		}
+
+		public long randLong() {
+			long a = rand.nextInt(), b = rand.nextInt();
+			return Math.abs(((a^(b >>> 16))<<32)|((a >>> 16)^b));
+		}
+
+		public long rand() {
+			return randLong()%(r-l+1)+l;
+		}
+
+		public void solve(int kase, InputReader in, Output pw) {
+			this.in = in;
+			this.pw = pw;
+			n = in.nextLong();
+			k = in.nextInt();
+			l = 1;
+			r = n;
+			do {
+				if(bsearch()) {
+					break;
+				}
+//				Utilities.Debug.dbg(l, r);
+				long query = rand();
+				pw.println(query, query);
+				pw.flush();
+				updateRange();
+			} while(in.next().charAt(0)=='N');
+		}
+
+	}
+
+	static interface InputReader {
+		String next();
+
+		int nextInt();
+
+		long nextLong();
+
+	}
+
+	static class Utilities {
+		public static class Debug {
+			public static final boolean LOCAL = System.getProperty("ONLINE_JUDGE")==null;
+
+			private static <T> String ts(T t) {
+				if(t==null) {
+					return "null";
+				}
+				try {
+					return ts((Iterable) t);
+				}catch(ClassCastException e) {
+					if(t instanceof int[]) {
+						String s = Arrays.toString((int[]) t);
+						return "{"+s.substring(1, s.length()-1)+"}";
+					}else if(t instanceof long[]) {
+						String s = Arrays.toString((long[]) t);
+						return "{"+s.substring(1, s.length()-1)+"}";
+					}else if(t instanceof char[]) {
+						String s = Arrays.toString((char[]) t);
+						return "{"+s.substring(1, s.length()-1)+"}";
+					}else if(t instanceof double[]) {
+						String s = Arrays.toString((double[]) t);
+						return "{"+s.substring(1, s.length()-1)+"}";
+					}else if(t instanceof boolean[]) {
+						String s = Arrays.toString((boolean[]) t);
+						return "{"+s.substring(1, s.length()-1)+"}";
+					}
+					try {
+						return ts((Object[]) t);
+					}catch(ClassCastException e1) {
+						return t.toString();
+					}
+				}
+			}
+
+			private static <T> String ts(T[] arr) {
+				StringBuilder ret = new StringBuilder();
+				ret.append("{");
+				boolean first = true;
+				for(T t: arr) {
+					if(!first) {
+						ret.append(", ");
+					}
+					first = false;
+					ret.append(ts(t));
+				}
+				ret.append("}");
+				return ret.toString();
+			}
+
+			private static <T> String ts(Iterable<T> iter) {
+				StringBuilder ret = new StringBuilder();
+				ret.append("{");
+				boolean first = true;
+				for(T t: iter) {
+					if(!first) {
+						ret.append(", ");
+					}
+					first = false;
+					ret.append(ts(t));
+				}
+				ret.append("}");
+				return ret.toString();
+			}
+
+			public static void dbg(Object... o) {
+				if(LOCAL) {
+					System.err.print("Line #"+Thread.currentThread().getStackTrace()[2].getLineNumber()+": [");
+					for(int i = 0; i<o.length; i++) {
+						if(i!=0) {
+							System.err.print(", ");
+						}
+						System.err.print(ts(o[i]));
+					}
+					System.err.println("]");
+				}
+			}
+
+		}
+
+	}
+
+	static class Output implements Closeable, Flushable {
+		public StringBuilder sb;
+		public OutputStream os;
+		public int BUFFER_SIZE;
+		public String lineSeparator;
+
+		public Output(OutputStream os) {
+			this(os, 1<<16);
+		}
+
+		public Output(OutputStream os, int bs) {
+			BUFFER_SIZE = bs;
+			sb = new StringBuilder(BUFFER_SIZE);
+			this.os = new BufferedOutputStream(os, 1<<17);
+			lineSeparator = System.lineSeparator();
+		}
+
+		public void print(String s) {
+			sb.append(s);
+			if(sb.length()>BUFFER_SIZE >> 1) {
+				flushToBuffer();
+			}
+		}
+
+		public void println(Object... o) {
+			for(int i = 0; i<o.length; i++) {
+				if(i!=0) {
+					print(" ");
+				}
+				print(String.valueOf(o[i]));
+			}
+			println();
+		}
+
+		public void println() {
+			sb.append(lineSeparator);
+		}
+
+		private void flushToBuffer() {
+			try {
+				os.write(sb.toString().getBytes());
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+			sb = new StringBuilder(BUFFER_SIZE);
+		}
+
+		public void flush() {
+			try {
+				flushToBuffer();
+				os.flush();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		public void close() {
+			flush();
+			try {
+				os.close();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	static class Input implements InputReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public Input(InputStream is) {
+			this(is, 1<<20);
+		}
+
+		public Input(InputStream is, int bs) {
+			br = new BufferedReader(new InputStreamReader(is), bs);
+			st = new StringTokenizer("");
+		}
+
+		public boolean hasNext() {
+			try {
+				while(st==null||!st.hasMoreTokens()) {
+					String s = br.readLine();
+					if(s==null) {
+						return false;
+					}
+					st = new StringTokenizer(s);
+				}
+				return true;
+			}catch(Exception e) {
+				return false;
+			}
+		}
+
+		public String next() {
+			if(!hasNext()) {
+				throw new InputMismatchException();
+			}
+			return st.nextToken();
+		}
+
+		public int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		public long nextLong() {
+			return Long.parseLong(next());
+		}
+
+	}
+}
+
