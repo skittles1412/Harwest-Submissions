510f66de441b80dfa8ece3b4cb91af1942cbe829
skittles1412 <61439840+skittles1412@users.noreply.github.com>
2020-10-14 17:54:00 -0700

    Add solution for problem `F - Decreasing Heights` Link: https://codeforces.com/contest/1353/problem/F Tags: brute force, dp, *2200 Ref: https://codeforces.com/contest/1353/submission/95547707

---
+++ codeforces/1353/F.java
@@ -0,0 +1,254 @@
+import java.io.BufferedOutputStream;
+import java.io.Closeable;
+import java.io.DataInputStream;
+import java.io.Flushable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+	static class TaskAdapter implements Runnable {
+		@Override
+		public void run() {
+			long startTime = System.currentTimeMillis();
+			InputStream inputStream = System.in;
+			OutputStream outputStream = System.out;
+			FastReader in = new FastReader(inputStream);
+			Output out = new Output(outputStream);
+			FDecreasingHeights solver = new FDecreasingHeights();
+			int testCount = in.nextInt();
+			for(int i = 1; i<=testCount; i++)
+				solver.solve(i, in, out);
+			out.close();
+			System.err.println(System.currentTimeMillis()-startTime+"ms");
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Thread thread = new Thread(null, new TaskAdapter(), "", 1<<28);
+		thread.start();
+		thread.join();
+	}
+
+	static class FDecreasingHeights {
+		private final long linf = 4_000_000_000_000_000_000L;
+		int n;
+		int m;
+		long[][] arr;
+
+		public FDecreasingHeights() {
+		}
+
+		public long dp() {
+			long[][] dp = new long[n+1][m+1];
+			Arrays.fill(dp[n], linf);
+			for(int i = 0; i<n; i++) {
+				dp[i][m] = linf;
+			}
+			dp[n-1][m] = 0;
+			for(int i = n-1; i>=0; i--) {
+				for(int j = m-1; j>=0; j--) {
+					long val = i+j;
+					if(arr[i][j]<val) {
+						dp[i][j] = linf;
+					}else {
+						dp[i][j] = Math.min(linf, arr[i][j]-val+Math.min(dp[i+1][j], dp[i][j+1]));
+					}
+				}
+			}
+			return dp[0][0];
+		}
+
+		public void add(long val) {
+			for(int i = 0; i<n; i++) {
+				for(int j = 0; j<m; j++) {
+					arr[i][j] += val;
+				}
+			}
+		}
+
+		public void solve(int kase, InputReader in, Output pw) {
+			n = in.nextInt();
+			m = in.nextInt();
+			arr = in.nextLong(n, m);
+			long ans = linf, val = arr[0][0];
+			for(int i = 0; i<n; i++) {
+				for(int j = 0; j<m; j++) {
+					long add = i+j-arr[i][j];
+					add(add);
+					ans = Math.min(ans, dp());
+					add(-add);
+				}
+			}
+			pw.println(ans);
+		}
+
+	}
+
+	static class FastReader implements InputReader {
+		final private int BUFFER_SIZE = 1<<16;
+		private DataInputStream din;
+		private byte[] buffer;
+		private int bufferPointer;
+		private int bytesRead;
+
+		public FastReader(InputStream is) {
+			din = new DataInputStream(is);
+			buffer = new byte[BUFFER_SIZE];
+			bufferPointer = bytesRead = 0;
+		}
+
+		public int nextInt() {
+			int ret = 0;
+			byte c = skipToDigit();
+			boolean neg = (c=='-');
+			if(neg) {
+				c = read();
+			}
+			do {
+				ret = ret*10+c-'0';
+			} while((c = read())>='0'&&c<='9');
+			if(neg) {
+				return -ret;
+			}
+			return ret;
+		}
+
+		public long nextLong() {
+			long ret = 0;
+			byte c = skipToDigit();
+			boolean neg = (c=='-');
+			if(neg) {
+				c = read();
+			}
+			do {
+				ret = ret*10+c-'0';
+			} while((c = read())>='0'&&c<='9');
+			if(neg) {
+				return -ret;
+			}
+			return ret;
+		}
+
+		private boolean isDigit(byte b) {
+			return b>='0'&&b<='9';
+		}
+
+		private byte skipToDigit() {
+			byte ret;
+			while(!isDigit(ret = read())&&ret!='-') ;
+			return ret;
+		}
+
+		private void fillBuffer() {
+			try {
+				bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+			}catch(IOException e) {
+				e.printStackTrace();
+				throw new InputMismatchException();
+			}
+			if(bytesRead==-1) {
+				buffer[0] = -1;
+			}
+		}
+
+		private byte read() {
+			if(bytesRead==-1) {
+				throw new InputMismatchException();
+			}else if(bufferPointer==bytesRead) {
+				fillBuffer();
+			}
+			return buffer[bufferPointer++];
+		}
+
+	}
+
+	static interface InputReader {
+		int nextInt();
+
+		long nextLong();
+
+		default long[] nextLong(int n) {
+			long[] ret = new long[n];
+			for(int i = 0; i<n; i++) {
+				ret[i] = nextLong();
+			}
+			return ret;
+		}
+
+		default long[][] nextLong(int n, int m) {
+			long[][] ret = new long[n][m];
+			for(int i = 0; i<n; i++) {
+				ret[i] = nextLong(m);
+			}
+			return ret;
+		}
+
+	}
+
+	static class Output implements Closeable, Flushable {
+		public StringBuilder sb;
+		public OutputStream os;
+		public int BUFFER_SIZE;
+		public String lineSeparator;
+
+		public Output(OutputStream os) {
+			this(os, 1<<16);
+		}
+
+		public Output(OutputStream os, int bs) {
+			BUFFER_SIZE = bs;
+			sb = new StringBuilder(BUFFER_SIZE);
+			this.os = new BufferedOutputStream(os, 1<<17);
+			lineSeparator = System.lineSeparator();
+		}
+
+		public void println(long l) {
+			println(String.valueOf(l));
+		}
+
+		public void println(String s) {
+			sb.append(s);
+			println();
+		}
+
+		public void println() {
+			sb.append(lineSeparator);
+		}
+
+		private void flushToBuffer() {
+			try {
+				os.write(sb.toString().getBytes());
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+			sb = new StringBuilder(BUFFER_SIZE);
+		}
+
+		public void flush() {
+			try {
+				flushToBuffer();
+				os.flush();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		public void close() {
+			flush();
+			try {
+				os.close();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+}
+
