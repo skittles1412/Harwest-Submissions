adcf9be2af4dcf2ab45d748f65419a32409ffb1c
skittles1412 <61439840+skittles1412@users.noreply.github.com>
2020-10-17 10:02:00 -0700

    Add solution for problem `E - Carrots for Rabbits` Link: https://codeforces.com/contest/1428/problem/E Tags: binary search, data structures, greedy, math, sortings, *2200 Ref: https://codeforces.com/contest/1428/submission/95814192

---
+++ codeforces/1428/E.java
@@ -0,0 +1,301 @@
+import java.io.BufferedOutputStream;
+import java.io.Closeable;
+import java.io.DataInputStream;
+import java.io.Flushable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.AbstractQueue;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.InputMismatchException;
+import java.util.PriorityQueue;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+	static class TaskAdapter implements Runnable {
+		@Override
+		public void run() {
+			long startTime = System.currentTimeMillis();
+			InputStream inputStream = System.in;
+			OutputStream outputStream = System.out;
+			FastReader in = new FastReader(inputStream);
+			Output out = new Output(outputStream);
+			ECarrotsForRabbits solver = new ECarrotsForRabbits();
+			solver.solve(1, in, out);
+			out.close();
+			System.err.println(System.currentTimeMillis()-startTime+"ms");
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Thread thread = new Thread(null, new TaskAdapter(), "", 1<<28);
+		thread.start();
+		thread.join();
+	}
+
+	static class ECarrotsForRabbits {
+		private final long linf = 4_000_000_000_000_000_000L;
+
+		public ECarrotsForRabbits() {
+		}
+
+		public long get(int x, int cnt) {
+			if(cnt>x) {
+				return linf;
+			}
+			long mod = x%cnt, div = x/cnt;
+			return mod*(div+1)*(div+1)+(cnt-mod)*div*div;
+		}
+
+		public void solve(int kase, InputReader in, Output pw) {
+			int n = in.nextInt(), k = in.nextInt();
+			int[] arr = in.nextInt(n), cnt = new int[n];
+			Arrays.fill(cnt, 2);
+			long[] val = new long[n], nval = new long[n];
+			PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.comparingLong(o -> nval[o]-val[o]));
+			for(int i = 0; i<n; i++) {
+				val[i] = (long) arr[i]*arr[i];
+				nval[i] = get(arr[i], 2);
+			}
+			for(int i = 0; i<n; i++) {
+				pq.add(i);
+			}
+			for(int i = n; i<k; i++) {
+				int cur = pq.remove();
+				val[cur] = nval[cur];
+				nval[cur] = get(arr[cur], ++cnt[cur]);
+				pq.add(cur);
+			}
+//			Utilities.Debug.dbg(val);
+//			Utilities.Debug.dbg(cnt);
+			long ans = 0;
+			for(int i = 0; i<n; i++) {
+				ans += val[i];
+			}
+			pw.println(ans);
+		}
+
+	}
+
+	static class FastReader implements InputReader {
+		final private int BUFFER_SIZE = 1<<16;
+		private DataInputStream din;
+		private byte[] buffer;
+		private int bufferPointer;
+		private int bytesRead;
+
+		public FastReader(InputStream is) {
+			din = new DataInputStream(is);
+			buffer = new byte[BUFFER_SIZE];
+			bufferPointer = bytesRead = 0;
+		}
+
+		public int nextInt() {
+			int ret = 0;
+			byte c = skipToDigit();
+			boolean neg = (c=='-');
+			if(neg) {
+				c = read();
+			}
+			do {
+				ret = ret*10+c-'0';
+			} while((c = read())>='0'&&c<='9');
+			if(neg) {
+				return -ret;
+			}
+			return ret;
+		}
+
+		private boolean isDigit(byte b) {
+			return b>='0'&&b<='9';
+		}
+
+		private byte skipToDigit() {
+			byte ret;
+			while(!isDigit(ret = read())&&ret!='-') ;
+			return ret;
+		}
+
+		private void fillBuffer() {
+			try {
+				bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+			}catch(IOException e) {
+				e.printStackTrace();
+				throw new InputMismatchException();
+			}
+			if(bytesRead==-1) {
+				buffer[0] = -1;
+			}
+		}
+
+		private byte read() {
+			if(bytesRead==-1) {
+				throw new InputMismatchException();
+			}else if(bufferPointer==bytesRead) {
+				fillBuffer();
+			}
+			return buffer[bufferPointer++];
+		}
+
+	}
+
+	static interface InputReader {
+		int nextInt();
+
+		default int[] nextInt(int n) {
+			int[] ret = new int[n];
+			for(int i = 0; i<n; i++) {
+				ret[i] = nextInt();
+			}
+			return ret;
+		}
+
+	}
+
+	static class Utilities {
+		public static class Debug {
+			public static final boolean LOCAL = System.getProperty("ONLINE_JUDGE")==null;
+
+			private static <T> String ts(T t) {
+				if(t==null) {
+					return "null";
+				}
+				try {
+					return ts((Iterable) t);
+				}catch(ClassCastException e) {
+					if(t instanceof int[]) {
+						String s = Arrays.toString((int[]) t);
+						return "{"+s.substring(1, s.length()-1)+"}";
+					}else if(t instanceof long[]) {
+						String s = Arrays.toString((long[]) t);
+						return "{"+s.substring(1, s.length()-1)+"}";
+					}else if(t instanceof char[]) {
+						String s = Arrays.toString((char[]) t);
+						return "{"+s.substring(1, s.length()-1)+"}";
+					}else if(t instanceof double[]) {
+						String s = Arrays.toString((double[]) t);
+						return "{"+s.substring(1, s.length()-1)+"}";
+					}else if(t instanceof boolean[]) {
+						String s = Arrays.toString((boolean[]) t);
+						return "{"+s.substring(1, s.length()-1)+"}";
+					}
+					try {
+						return ts((Object[]) t);
+					}catch(ClassCastException e1) {
+						return t.toString();
+					}
+				}
+			}
+
+			private static <T> String ts(T[] arr) {
+				StringBuilder ret = new StringBuilder();
+				ret.append("{");
+				boolean first = true;
+				for(T t: arr) {
+					if(!first) {
+						ret.append(", ");
+					}
+					first = false;
+					ret.append(ts(t));
+				}
+				ret.append("}");
+				return ret.toString();
+			}
+
+			private static <T> String ts(Iterable<T> iter) {
+				StringBuilder ret = new StringBuilder();
+				ret.append("{");
+				boolean first = true;
+				for(T t: iter) {
+					if(!first) {
+						ret.append(", ");
+					}
+					first = false;
+					ret.append(ts(t));
+				}
+				ret.append("}");
+				return ret.toString();
+			}
+
+			public static void dbg(Object... o) {
+				if(LOCAL) {
+					System.err.print("Line #"+Thread.currentThread().getStackTrace()[2].getLineNumber()+": [");
+					for(int i = 0; i<o.length; i++) {
+						if(i!=0) {
+							System.err.print(", ");
+						}
+						System.err.print(ts(o[i]));
+					}
+					System.err.println("]");
+				}
+			}
+
+		}
+
+	}
+
+	static class Output implements Closeable, Flushable {
+		public StringBuilder sb;
+		public OutputStream os;
+		public int BUFFER_SIZE;
+		public String lineSeparator;
+
+		public Output(OutputStream os) {
+			this(os, 1<<16);
+		}
+
+		public Output(OutputStream os, int bs) {
+			BUFFER_SIZE = bs;
+			sb = new StringBuilder(BUFFER_SIZE);
+			this.os = new BufferedOutputStream(os, 1<<17);
+			lineSeparator = System.lineSeparator();
+		}
+
+		public void println(long l) {
+			println(String.valueOf(l));
+		}
+
+		public void println(String s) {
+			sb.append(s);
+			println();
+		}
+
+		public void println() {
+			sb.append(lineSeparator);
+		}
+
+		private void flushToBuffer() {
+			try {
+				os.write(sb.toString().getBytes());
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+			sb = new StringBuilder(BUFFER_SIZE);
+		}
+
+		public void flush() {
+			try {
+				flushToBuffer();
+				os.flush();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		public void close() {
+			flush();
+			try {
+				os.close();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+}
+
