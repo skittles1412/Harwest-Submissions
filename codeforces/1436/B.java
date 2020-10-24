67d76162a7c7175db97371d0ef140d0b8f36b015
skittles1412 <61439840+skittles1412@users.noreply.github.com>
2020-10-24 07:18:00 -0700

    Add solution for problem `B - Prime Square` Link: https://codeforces.com/contest/1436/problem/B Tags: constructive algorithms, math, *900 Ref: https://codeforces.com/contest/1436/submission/96544270

---
+++ codeforces/1436/B.java
@@ -0,0 +1,234 @@
+import java.io.BufferedOutputStream;
+import java.io.Closeable;
+import java.io.DataInputStream;
+import java.io.Flushable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.InputMismatchException;
+import java.util.LinkedHashSet;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+	static class TaskAdapter implements Runnable {
+		@Override
+		public void run() {
+			long startTime = System.currentTimeMillis();
+			InputStream inputStream = System.in;
+			OutputStream outputStream = System.out;
+			FastReader in = new FastReader(inputStream);
+			Output out = new Output(outputStream);
+			BPrimeSquare solver = new BPrimeSquare();
+			int testCount = in.nextInt();
+			for(int i = 1; i<=testCount; i++)
+				solver.solve(i, in, out);
+			out.close();
+			System.err.println(System.currentTimeMillis()-startTime+"ms");
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Thread thread = new Thread(null, new TaskAdapter(), "", 1<<28);
+		thread.start();
+		thread.join();
+	}
+
+	static class BPrimeSquare {
+		LinkedHashSet<Integer> prime;
+
+		public BPrimeSquare() {
+			ArrayList<Integer> sieve = Utilities.sieve(100000);
+			Collections.reverse(sieve);
+			prime = new LinkedHashSet<>(sieve);
+		}
+
+		public void solve(int kase, InputReader in, Output pw) {
+			int n = in.nextInt();
+			int[][] arr = new int[n][n];
+			for(int i = 0; i<n; i++) {
+				for(int j = 0; j<n; j++) {
+					arr[i][j] = 1;
+				}
+			}
+			for(int i: prime) {
+				if(!prime.contains(i-n+1)) {
+					for(int j = 0; j<n; j++) {
+						arr[j][j] = i-n+1;
+					}
+					break;
+				}
+			}
+			for(int i = 0; i<n; i++) {
+				pw.println(arr[i]);
+			}
+		}
+
+	}
+
+	static class Output implements Closeable, Flushable {
+		public StringBuilder sb;
+		public OutputStream os;
+		public int BUFFER_SIZE;
+		public String lineSeparator;
+
+		public Output(OutputStream os) {
+			this(os, 1<<16);
+		}
+
+		public Output(OutputStream os, int bs) {
+			BUFFER_SIZE = bs;
+			sb = new StringBuilder(BUFFER_SIZE);
+			this.os = new BufferedOutputStream(os, 1<<17);
+			lineSeparator = System.lineSeparator();
+		}
+
+		public void print(int i) {
+			print(String.valueOf(i));
+		}
+
+		public void print(String s) {
+			sb.append(s);
+			if(sb.length()>BUFFER_SIZE >> 1) {
+				flushToBuffer();
+			}
+		}
+
+		public void println() {
+			sb.append(lineSeparator);
+		}
+
+		public void println(int[] arr) {
+			for(int i = 0; i<arr.length; i++) {
+				if(i!=0) {
+					print(" ");
+				}
+				print(arr[i]);
+			}
+			println();
+		}
+
+		private void flushToBuffer() {
+			try {
+				os.write(sb.toString().getBytes());
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+			sb = new StringBuilder(BUFFER_SIZE);
+		}
+
+		public void flush() {
+			try {
+				flushToBuffer();
+				os.flush();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		public void close() {
+			flush();
+			try {
+				os.close();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	static interface InputReader {
+		int nextInt();
+
+	}
+
+	static class FastReader implements InputReader {
+		final private int BUFFER_SIZE = 1<<16;
+		private DataInputStream din;
+		private byte[] buffer;
+		private int bufferPointer;
+		private int bytesRead;
+
+		public FastReader(InputStream is) {
+			din = new DataInputStream(is);
+			buffer = new byte[BUFFER_SIZE];
+			bufferPointer = bytesRead = 0;
+		}
+
+		public int nextInt() {
+			int ret = 0;
+			byte c = skipToDigit();
+			boolean neg = (c=='-');
+			if(neg) {
+				c = read();
+			}
+			do {
+				ret = ret*10+c-'0';
+			} while((c = read())>='0'&&c<='9');
+			if(neg) {
+				return -ret;
+			}
+			return ret;
+		}
+
+		private boolean isDigit(byte b) {
+			return b>='0'&&b<='9';
+		}
+
+		private byte skipToDigit() {
+			byte ret;
+			while(!isDigit(ret = read())&&ret!='-') ;
+			return ret;
+		}
+
+		private void fillBuffer() {
+			try {
+				bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+			}catch(IOException e) {
+				e.printStackTrace();
+				throw new InputMismatchException();
+			}
+			if(bytesRead==-1) {
+				buffer[0] = -1;
+			}
+		}
+
+		private byte read() {
+			if(bytesRead==-1) {
+				throw new InputMismatchException();
+			}else if(bufferPointer==bytesRead) {
+				fillBuffer();
+			}
+			return buffer[bufferPointer++];
+		}
+
+	}
+
+	static class Utilities {
+		public static ArrayList<Integer> sieve(int n) {
+			ArrayList<Integer> ans = new ArrayList<>();
+			boolean[] prime = new boolean[n+1];
+			Arrays.fill(prime, true);
+			for(int i = 2; i<=n; i++) {
+				if(prime[i]) {
+					ans.add(i);
+				}
+				if((long) i*i>n) {
+					continue;
+				}
+				for(int j = i*i; j<=n; j += i) {
+					prime[j] = false;
+				}
+			}
+			return ans;
+		}
+
+	}
+}
+
