efbfd2492801898c74cffdcd88fdc6851f26a4cc
skittles1412 <61439840+skittles1412@users.noreply.github.com>
2020-10-31 14:14:00 -0700

    Add solution for problem `E - String Reversal` Link: https://codeforces.com/contest/1430/problem/E Tags: data structures, greedy, strings, *1900 Ref: https://codeforces.com/contest/1430/submission/97270981

---
+++ codeforces/1430/E.java
@@ -0,0 +1,250 @@
+import java.io.BufferedOutputStream;
+import java.io.Closeable;
+import java.io.DataInputStream;
+import java.io.Flushable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayDeque;
+import java.util.InputMismatchException;
+import java.util.function.Function;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+	static class TaskAdapter implements Runnable {
+		@Override
+		public void run() {
+			long startTime = System.currentTimeMillis();
+			InputStream inputStream = System.in;
+			OutputStream outputStream = System.out;
+			FastReader in = new FastReader(inputStream);
+			Output out = new Output(outputStream);
+			EStringReversal solver = new EStringReversal();
+			solver.solve(1, in, out);
+			out.close();
+			System.err.println(System.currentTimeMillis()-startTime+"ms");
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Thread thread = new Thread(null, new TaskAdapter(), "", 1<<28);
+		thread.start();
+		thread.join();
+	}
+
+	static class EStringReversal {
+		public EStringReversal() {
+		}
+
+		public void solve(int kase, InputReader in, Output pw) {
+			int n = in.nextInt();
+			int[] arr = in.nextIntChar(o -> o-'a');
+			ArrayDeque<Integer>[] index = new ArrayDeque[26];
+			for(int i = 0; i<26; i++) {
+				index[i] = new ArrayDeque<>();
+			}
+			for(int i = 0; i<n; i++) {
+				index[arr[i]].addLast(i);
+			}
+			int[] move = new int[n];
+			for(int i = 0; i<n; i++) {
+				move[index[arr[n-i-1]].removeFirst()] = i;
+			}
+			pw.println(Utilities.countInversions(move));
+		}
+
+	}
+
+	static class Output implements Closeable, Flushable {
+		public StringBuilder sb;
+		public OutputStream os;
+		public int BUFFER_SIZE;
+		public String lineSeparator;
+
+		public Output(OutputStream os) {
+			this(os, 1<<16);
+		}
+
+		public Output(OutputStream os, int bs) {
+			BUFFER_SIZE = bs;
+			sb = new StringBuilder(BUFFER_SIZE);
+			this.os = new BufferedOutputStream(os, 1<<17);
+			lineSeparator = System.lineSeparator();
+		}
+
+		public void println(long l) {
+			println(String.valueOf(l));
+		}
+
+		public void println(String s) {
+			sb.append(s);
+			println();
+		}
+
+		public void println() {
+			sb.append(lineSeparator);
+		}
+
+		private void flushToBuffer() {
+			try {
+				os.write(sb.toString().getBytes());
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+			sb = new StringBuilder(BUFFER_SIZE);
+		}
+
+		public void flush() {
+			try {
+				flushToBuffer();
+				os.flush();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		public void close() {
+			flush();
+			try {
+				os.close();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	static interface InputReader {
+		String next();
+
+		int nextInt();
+
+		default int[] nextIntChar(Function<Character, Integer> f) {
+			String s = next();
+			int[] ret = new int[s.length()];
+			for(int i = 0; i<s.length(); i++) {
+				ret[i] = f.apply(s.charAt(i));
+			}
+			return ret;
+		}
+
+	}
+
+	static class Utilities {
+		public static long countInversions(int[] arr) {
+			return countInversions(arr.clone(), 0, arr.length-1);
+		}
+
+		public static long countInversions(int[] arr, int l, int r) {
+			if(l>=r) {
+				return 0;
+			}
+			int mid = l+r >> 1, larr[] = new int[mid-l+2], rarr[] = new int[r-mid+1];
+			long ans = countInversions(arr, l, mid)+countInversions(arr, mid+1, r);
+			for(int i = l; i<=mid; i++) {
+				larr[i-l] = arr[i];
+			}
+			for(int i = mid+1; i<=r; i++) {
+				rarr[i-mid-1] = arr[i];
+			}
+			larr[mid-l+1] = rarr[r-mid] = Integer.MAX_VALUE;
+			int i = 0, j = 0, ind = l;
+			while(ind<=r) {
+				if(larr[i]<=rarr[j]) {
+					arr[ind++] = larr[i++];
+					ans += j;
+				}else {
+					arr[ind++] = rarr[j++];
+				}
+			}
+			return ans;
+		}
+
+	}
+
+	static class FastReader implements InputReader {
+		final private int BUFFER_SIZE = 1<<16;
+		private DataInputStream din;
+		private byte[] buffer;
+		private int bufferPointer;
+		private int bytesRead;
+
+		public FastReader(InputStream is) {
+			din = new DataInputStream(is);
+			buffer = new byte[BUFFER_SIZE];
+			bufferPointer = bytesRead = 0;
+		}
+
+		public String next() {
+			StringBuilder ret = new StringBuilder(64);
+			byte c = skip();
+			while(c!=-1&&!isSpaceChar(c)) {
+				ret.appendCodePoint(c);
+				c = read();
+			}
+			return ret.toString();
+		}
+
+		public int nextInt() {
+			int ret = 0;
+			byte c = skipToDigit();
+			boolean neg = (c=='-');
+			if(neg) {
+				c = read();
+			}
+			do {
+				ret = ret*10+c-'0';
+			} while((c = read())>='0'&&c<='9');
+			if(neg) {
+				return -ret;
+			}
+			return ret;
+		}
+
+		private boolean isSpaceChar(byte b) {
+			return b==' '||b=='\r'||b=='\n'||b=='\t'||b=='\f';
+		}
+
+		private byte skip() {
+			byte ret;
+			while(isSpaceChar((ret = read()))) ;
+			return ret;
+		}
+
+		private boolean isDigit(byte b) {
+			return b>='0'&&b<='9';
+		}
+
+		private byte skipToDigit() {
+			byte ret;
+			while(!isDigit(ret = read())&&ret!='-') ;
+			return ret;
+		}
+
+		private void fillBuffer() {
+			try {
+				bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+			}catch(IOException e) {
+				e.printStackTrace();
+				throw new InputMismatchException();
+			}
+			if(bytesRead==-1) {
+				buffer[0] = -1;
+			}
+		}
+
+		private byte read() {
+			if(bytesRead==-1) {
+				throw new InputMismatchException();
+			}else if(bufferPointer==bytesRead) {
+				fillBuffer();
+			}
+			return buffer[bufferPointer++];
+		}
+
+	}
+}
+
