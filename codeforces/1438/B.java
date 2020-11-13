aef77a9aa9ce84b2fe8a547942fdac62f0bf5872
skittles1412 <61439840+skittles1412@users.noreply.github.com>
2020-11-13 06:56:00 -0800

    Add solution for problem `B - Valerii Against Everyone` Link: https://codeforces.com/contest/1438/problem/B Tags: constructive algorithms, data structures, greedy, sortings, *1000 Ref: https://codeforces.com/contest/1438/submission/98297105

---
+++ codeforces/1438/B.java
@@ -0,0 +1,261 @@
+import java.io.BufferedOutputStream;
+import java.io.Closeable;
+import java.io.DataInputStream;
+import java.io.Flushable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.InputMismatchException;
+import java.util.Objects;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+	static class TaskAdapter implements Runnable {
+		@Override
+		public void run() {
+			long startTime = System.currentTimeMillis();
+			InputStream inputStream = System.in;
+			OutputStream outputStream = System.out;
+			FastReader in = new FastReader(inputStream);
+			Output out = new Output(outputStream);
+			BValeriiAgainstEveryone solver = new BValeriiAgainstEveryone();
+			int testCount = in.nextInt();
+			for(int i = 1; i<=testCount; i++)
+				solver.solve(i, in, out);
+			out.close();
+			System.err.println(System.currentTimeMillis()-startTime+"ms");
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Thread thread = new Thread(null, new TaskAdapter(), "", 1<<28);
+		thread.start();
+		thread.join();
+	}
+
+	static class BValeriiAgainstEveryone {
+		public BValeriiAgainstEveryone() {
+		}
+
+		public void solve(int kase, InputReader in, Output pw) {
+			int n = in.nextInt();
+			int[] arr = in.nextInt(n);
+			HashMap<Integer, Pair<Integer, Integer>> min = new HashMap<>(), max = new HashMap<>();
+			for(int i = 0; i<n; i++) {
+				HashSet<Integer> cnt = new HashSet<>();
+				for(int j = i; j<n; j++) {
+					int cur = arr[j];
+					while(cnt.contains(cur)) {
+						cnt.remove(cur);
+						cur++;
+					}
+					cnt.add(cur);
+					if(cnt.size()==1) {
+						var v = new Pair<>(i, j);
+						if(!min.containsKey(cur)) {
+							min.put(cur, v);
+						}else {
+							var c = min.get(cur);
+							if(j<c.b) {
+								c.a = i;
+								c.b = j;
+							}else if(i>c.b) {
+								pw.println("YES");
+								return;
+							}
+						}
+						if(!max.containsKey(cur)) {
+							max.put(cur, v);
+						}else {
+							var c = max.get(cur);
+							if(i>c.a) {
+								c.a = i;
+								c.b = j;
+							}else if(j<c.a) {
+								pw.println("YES");
+								return;
+							}
+						}
+					}
+				}
+			}
+			pw.println("NO");
+		}
+
+	}
+
+	static interface InputReader {
+		int nextInt();
+
+		default int[] nextInt(int n) {
+			int[] ret = new int[n];
+			for(int i = 0; i<n; i++) {
+				ret[i] = nextInt();
+			}
+			return ret;
+		}
+
+	}
+
+	static class Pair<T1, T2> implements Comparable<Pair<T1, T2>> {
+		public T1 a;
+		public T2 b;
+
+		public Pair(Pair<T1, T2> p) {
+			this(p.a, p.b);
+		}
+
+		public Pair(T1 a, T2 b) {
+			this.a = a;
+			this.b = b;
+		}
+
+		public String toString() {
+			return a+" "+b;
+		}
+
+		public int hashCode() {
+			return Objects.hash(a, b);
+		}
+
+		public boolean equals(Object o) {
+			if(o instanceof Pair) {
+				Pair p = (Pair) o;
+				return a.equals(p.a)&&b.equals(p.b);
+			}
+			return false;
+		}
+
+		public int compareTo(Pair<T1, T2> p) {
+			int cmp = ((Comparable<T1>) a).compareTo(p.a);
+			if(cmp==0) {
+				return ((Comparable<T2>) b).compareTo(p.b);
+			}
+			return cmp;
+		}
+
+	}
+
+	static class Output implements Closeable, Flushable {
+		public StringBuilder sb;
+		public OutputStream os;
+		public int BUFFER_SIZE;
+		public String lineSeparator;
+
+		public Output(OutputStream os) {
+			this(os, 1<<16);
+		}
+
+		public Output(OutputStream os, int bs) {
+			BUFFER_SIZE = bs;
+			sb = new StringBuilder(BUFFER_SIZE);
+			this.os = new BufferedOutputStream(os, 1<<17);
+			lineSeparator = System.lineSeparator();
+		}
+
+		public void println(String s) {
+			sb.append(s);
+			println();
+		}
+
+		public void println() {
+			sb.append(lineSeparator);
+		}
+
+		private void flushToBuffer() {
+			try {
+				os.write(sb.toString().getBytes());
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+			sb = new StringBuilder(BUFFER_SIZE);
+		}
+
+		public void flush() {
+			try {
+				flushToBuffer();
+				os.flush();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		public void close() {
+			flush();
+			try {
+				os.close();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	static class FastReader implements InputReader {
+		final private int BUFFER_SIZE = 1<<16;
+		private DataInputStream din;
+		private byte[] buffer;
+		private int bufferPointer;
+		private int bytesRead;
+
+		public FastReader(InputStream is) {
+			din = new DataInputStream(is);
+			buffer = new byte[BUFFER_SIZE];
+			bufferPointer = bytesRead = 0;
+		}
+
+		public int nextInt() {
+			int ret = 0;
+			byte c = skipToDigit();
+			boolean neg = (c=='-');
+			if(neg) {
+				c = read();
+			}
+			do {
+				ret = ret*10+c-'0';
+			} while((c = read())>='0'&&c<='9');
+			if(neg) {
+				return -ret;
+			}
+			return ret;
+		}
+
+		private boolean isDigit(byte b) {
+			return b>='0'&&b<='9';
+		}
+
+		private byte skipToDigit() {
+			byte ret;
+			while(!isDigit(ret = read())&&ret!='-') ;
+			return ret;
+		}
+
+		private void fillBuffer() {
+			try {
+				bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+			}catch(IOException e) {
+				e.printStackTrace();
+				throw new InputMismatchException();
+			}
+			if(bytesRead==-1) {
+				buffer[0] = -1;
+			}
+		}
+
+		private byte read() {
+			if(bytesRead==-1) {
+				throw new InputMismatchException();
+			}else if(bufferPointer==bytesRead) {
+				fillBuffer();
+			}
+			return buffer[bufferPointer++];
+		}
+
+	}
+}
+
