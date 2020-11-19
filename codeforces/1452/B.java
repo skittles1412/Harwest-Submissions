656892dea4ed2e2a599157ffb219e7f92ee1ba7e
skittles1412 <61439840+skittles1412@users.noreply.github.com>
2020-11-19 06:45:00 -0800

    Add solution for problem `B - Toy Blocks` Link: https://codeforces.com/contest/1452/problem/B Tags: binary search, greedy, math, sortings, *1400 Ref: https://codeforces.com/contest/1452/submission/98900973

---
+++ codeforces/1452/B.java
@@ -0,0 +1,244 @@
+import java.io.BufferedOutputStream;
+import java.io.Closeable;
+import java.io.DataInputStream;
+import java.io.Flushable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.InputMismatchException;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+	static class TaskAdapter implements Runnable {
+		@Override
+		public void run() {
+			long startTime = System.currentTimeMillis();
+			InputStream inputStream = System.in;
+			OutputStream outputStream = System.out;
+			FastReader in = new FastReader(inputStream);
+			Output out = new Output(outputStream);
+			BToyBlocks solver = new BToyBlocks();
+			int testCount = in.nextInt();
+			for(int i = 1; i<=testCount; i++)
+				solver.solve(i, in, out);
+			out.close();
+			System.err.println(System.currentTimeMillis()-startTime+"ms");
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Thread thread = new Thread(null, new TaskAdapter(), "", 1<<28);
+		thread.start();
+		thread.join();
+	}
+
+	static class BToyBlocks {
+		public BToyBlocks() {
+		}
+
+		public void solve(int kase, InputReader in, Output pw) {
+			int n = in.nextInt();
+			long[] arr = in.nextLong(n);
+			TreeMap<Long, Integer> tm = new TreeMap<>();
+			long sum = 0;
+			for(int i = 0; i<n; i++) {
+				sum += arr[i];
+				Utilities.add(tm, arr[i]);
+			}
+			long min = (sum+n-2)/(n-1)*(n-1);
+			long ans = 0;
+			for(int i = 0; i<n; i++) {
+				Utilities.remove(tm, arr[i]);
+				ans = Math.max(ans, Math.max((n-1)*tm.lastKey(), min)-sum);
+				Utilities.add(tm, arr[i]);
+			}
+			pw.println(ans);
+		}
+
+	}
+
+	static class Output implements Closeable, Flushable {
+		public StringBuilder sb;
+		public OutputStream os;
+		public int BUFFER_SIZE;
+		public String lineSeparator;
+
+		public Output(OutputStream os) {
+			this(os, 1<<16);
+		}
+
+		public Output(OutputStream os, int bs) {
+			BUFFER_SIZE = bs;
+			sb = new StringBuilder(BUFFER_SIZE);
+			this.os = new BufferedOutputStream(os, 1<<17);
+			lineSeparator = System.lineSeparator();
+		}
+
+		public void println(long l) {
+			println(String.valueOf(l));
+		}
+
+		public void println(String s) {
+			sb.append(s);
+			println();
+		}
+
+		public void println() {
+			sb.append(lineSeparator);
+		}
+
+		private void flushToBuffer() {
+			try {
+				os.write(sb.toString().getBytes());
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+			sb = new StringBuilder(BUFFER_SIZE);
+		}
+
+		public void flush() {
+			try {
+				flushToBuffer();
+				os.flush();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		public void close() {
+			flush();
+			try {
+				os.close();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	static class Utilities {
+		public static <K, V> V getOrDefault(Map<? extends K, ? extends V> map, K key, V value) {
+			V ret = map.get(key);
+			if(ret==null) {
+				return value;
+			}
+			return ret;
+		}
+
+		public static <T> int add(Map<? super T, Integer> map, T key) {
+			int ret;
+			map.put(key, ret = getOrDefault(map, key, 0)+1);
+			return ret;
+		}
+
+		public static <T> int remove(Map<? super T, Integer> map, T key) {
+			int cnt = getOrDefault(map, key, 1)-1;
+			if(cnt>0) {
+				map.put(key, cnt);
+			}else {
+				map.remove(key);
+			}
+			return cnt;
+		}
+
+	}
+
+	static class FastReader implements InputReader {
+		final private int BUFFER_SIZE = 1<<16;
+		private DataInputStream din;
+		private byte[] buffer;
+		private int bufferPointer;
+		private int bytesRead;
+
+		public FastReader(InputStream is) {
+			din = new DataInputStream(is);
+			buffer = new byte[BUFFER_SIZE];
+			bufferPointer = bytesRead = 0;
+		}
+
+		public int nextInt() {
+			int ret = 0;
+			byte c = skipToDigit();
+			boolean neg = (c=='-');
+			if(neg) {
+				c = read();
+			}
+			do {
+				ret = ret*10+c-'0';
+			} while((c = read())>='0'&&c<='9');
+			if(neg) {
+				return -ret;
+			}
+			return ret;
+		}
+
+		public long nextLong() {
+			long ret = 0;
+			byte c = skipToDigit();
+			boolean neg = (c=='-');
+			if(neg) {
+				c = read();
+			}
+			do {
+				ret = ret*10+c-'0';
+			} while((c = read())>='0'&&c<='9');
+			if(neg) {
+				return -ret;
+			}
+			return ret;
+		}
+
+		private boolean isDigit(byte b) {
+			return b>='0'&&b<='9';
+		}
+
+		private byte skipToDigit() {
+			byte ret;
+			while(!isDigit(ret = read())&&ret!='-') ;
+			return ret;
+		}
+
+		private void fillBuffer() {
+			try {
+				bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+			}catch(IOException e) {
+				e.printStackTrace();
+				throw new InputMismatchException();
+			}
+			if(bytesRead==-1) {
+				buffer[0] = -1;
+			}
+		}
+
+		private byte read() {
+			if(bytesRead==-1) {
+				throw new InputMismatchException();
+			}else if(bufferPointer==bytesRead) {
+				fillBuffer();
+			}
+			return buffer[bufferPointer++];
+		}
+
+	}
+
+	static interface InputReader {
+		int nextInt();
+
+		long nextLong();
+
+		default long[] nextLong(int n) {
+			long[] ret = new long[n];
+			for(int i = 0; i<n; i++) {
+				ret[i] = nextLong();
+			}
+			return ret;
+		}
+
+	}
+}
+
