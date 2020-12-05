c4c2cfb23d4df35a72e9e58a33a3907850b3dfe8
skittles1412 <61439840+skittles1412@users.noreply.github.com>
2020-12-05 12:03:00 -0800

    Add solution for problem `A - Johnny and Contribution` Link: https://codeforces.com/contest/1361/problem/A Tags: constructive algorithms, graphs, greedy, sortings, *1700 Ref: https://codeforces.com/contest/1361/submission/100478328

---
+++ codeforces/1361/A.java
@@ -0,0 +1,251 @@
+import java.io.BufferedOutputStream;
+import java.io.Closeable;
+import java.io.DataInputStream;
+import java.io.Flushable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.InputMismatchException;
+import java.util.function.IntUnaryOperator;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+	static class TaskAdapter implements Runnable {
+		@Override
+		public void run() {
+			long startTime = System.currentTimeMillis();
+			InputStream inputStream = System.in;
+			OutputStream outputStream = System.out;
+			FastReader in = new FastReader(inputStream);
+			Output out = new Output(outputStream);
+			AJohnnyAndContribution solver = new AJohnnyAndContribution();
+			solver.solve(1, in, out);
+			out.close();
+			System.err.println(System.currentTimeMillis()-startTime+"ms");
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Thread thread = new Thread(null, new TaskAdapter(), "", 1<<28);
+		thread.start();
+		thread.join();
+	}
+
+	static class AJohnnyAndContribution {
+		public AJohnnyAndContribution() {
+		}
+
+		public void solve(int kase, InputReader in, Output pw) {
+			int n = in.nextInt(), m = in.nextInt();
+			ArrayList<Integer>[] graph = in.nextUndirectedGraph(n, m);
+			int[] arr = in.nextInt(n, o -> o-1);
+			ArrayDeque<Integer>[] ind = new ArrayDeque[n];
+			for(int i = 0; i<n; i++) {
+				ind[i] = new ArrayDeque<>();
+			}
+			for(int i = 0; i<n; i++) {
+				ind[arr[i]].addLast(i);
+			}
+			HashSet<Integer>[] visited = new HashSet[n];
+			for(int i = 0; i<n; i++) {
+				visited[i] = new HashSet<>();
+			}
+			ArrayList<Integer> ans = new ArrayList<>();
+			for(int i = 0; i<n; i++) {
+				for(int j: ind[i]) {
+					if(visited[j].size()==i&&!visited[j].contains(i)) {
+						for(int k: graph[j]) {
+							if(i<=arr[k]) {
+								visited[k].add(i);
+							}
+						}
+						ans.add(j+1);
+					}else {
+						pw.println("-1");
+						return;
+					}
+				}
+			}
+			pw.println(ans);
+		}
+
+	}
+
+	static interface InputReader {
+		int nextInt();
+
+		default int[] nextInt(int n, IntUnaryOperator operator) {
+			int[] ret = new int[n];
+			for(int i = 0; i<n; i++) {
+				ret[i] = operator.applyAsInt(nextInt());
+			}
+			return ret;
+		}
+
+		default ArrayList<Integer>[] nextUndirectedGraph(int n, int m) {
+			ArrayList<Integer>[] ret = new ArrayList[n];
+			for(int i = 0; i<n; i++) {
+				ret[i] = new ArrayList<>();
+			}
+			for(int i = 0; i<m; i++) {
+				int u = nextInt()-1, v = nextInt()-1;
+				ret[u].add(v);
+				ret[v].add(u);
+			}
+			return ret;
+		}
+
+	}
+
+	static class Output implements Closeable, Flushable {
+		public StringBuilder sb;
+		public OutputStream os;
+		public int BUFFER_SIZE;
+		public String lineSeparator;
+
+		public Output(OutputStream os) {
+			this(os, 1<<16);
+		}
+
+		public Output(OutputStream os, int bs) {
+			BUFFER_SIZE = bs;
+			sb = new StringBuilder(BUFFER_SIZE);
+			this.os = new BufferedOutputStream(os, 1<<17);
+			lineSeparator = System.lineSeparator();
+		}
+
+		public void print(Object... o) {
+			for(int i = 0; i<o.length; i++) {
+				if(i!=0) {
+					print(" ");
+				}
+				print(String.valueOf(o[i]));
+			}
+		}
+
+		public void print(String s) {
+			sb.append(s);
+			if(sb.length()>BUFFER_SIZE >> 1) {
+				flushToBuffer();
+			}
+		}
+
+		public void println(String s) {
+			sb.append(s);
+			println();
+		}
+
+		public void println() {
+			sb.append(lineSeparator);
+		}
+
+		public <T> void println(Iterable<T> iter) {
+			boolean first = true;
+			for(T t: iter) {
+				if(!first) {
+					print(" ");
+				}
+				first = false;
+				print(t);
+			}
+			println();
+		}
+
+		private void flushToBuffer() {
+			try {
+				os.write(sb.toString().getBytes());
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+			sb = new StringBuilder(BUFFER_SIZE);
+		}
+
+		public void flush() {
+			try {
+				flushToBuffer();
+				os.flush();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		public void close() {
+			flush();
+			try {
+				os.close();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	static class FastReader implements InputReader {
+		final private int BUFFER_SIZE = 1<<16;
+		private DataInputStream din;
+		private byte[] buffer;
+		private int bufferPointer;
+		private int bytesRead;
+
+		public FastReader(InputStream is) {
+			din = new DataInputStream(is);
+			buffer = new byte[BUFFER_SIZE];
+			bufferPointer = bytesRead = 0;
+		}
+
+		public int nextInt() {
+			int ret = 0;
+			byte c = skipToDigit();
+			boolean neg = (c=='-');
+			if(neg) {
+				c = read();
+			}
+			do {
+				ret = ret*10+c-'0';
+			} while((c = read())>='0'&&c<='9');
+			if(neg) {
+				return -ret;
+			}
+			return ret;
+		}
+
+		private boolean isDigit(byte b) {
+			return b>='0'&&b<='9';
+		}
+
+		private byte skipToDigit() {
+			byte ret;
+			while(!isDigit(ret = read())&&ret!='-') ;
+			return ret;
+		}
+
+		private void fillBuffer() {
+			try {
+				bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+			}catch(IOException e) {
+				e.printStackTrace();
+				throw new InputMismatchException();
+			}
+			if(bytesRead==-1) {
+				buffer[0] = -1;
+			}
+		}
+
+		private byte read() {
+			if(bytesRead==-1) {
+				throw new InputMismatchException();
+			}else if(bufferPointer==bytesRead) {
+				fillBuffer();
+			}
+			return buffer[bufferPointer++];
+		}
+
+	}
+}
+
