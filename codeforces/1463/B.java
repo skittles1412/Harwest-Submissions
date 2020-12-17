6e38e99423105058b999f7708ee642ebf0aeaabf
skittles1412 <61439840+skittles1412@users.noreply.github.com>
2020-12-17 06:52:00 -0800

    Add solution for problem `B - Find The Array` Link: https://codeforces.com/contest/1463/problem/B Tags: bitmasks, constructive algorithms, greedy, *1400 Ref: https://codeforces.com/contest/1463/submission/101525655

---
+++ codeforces/1463/B.java
@@ -0,0 +1,220 @@
+import java.io.BufferedOutputStream;
+import java.io.Closeable;
+import java.io.DataInputStream;
+import java.io.Flushable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.InputMismatchException;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+	static class TaskAdapter implements Runnable {
+		@Override
+		public void run() {
+			long startTime = System.currentTimeMillis();
+			InputStream inputStream = System.in;
+			OutputStream outputStream = System.out;
+			FastReader in = new FastReader(inputStream);
+			Output out = new Output(outputStream);
+			BFindTheArray solver = new BFindTheArray();
+			int testCount = in.nextInt();
+			for(int i = 1; i<=testCount; i++)
+				solver.solve(i, in, out);
+			out.close();
+			System.err.println(System.currentTimeMillis()-startTime+"ms");
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Thread thread = new Thread(null, new TaskAdapter(), "", 1<<28);
+		thread.start();
+		thread.join();
+	}
+
+	static class BFindTheArray {
+		public BFindTheArray() {
+		}
+
+		public void solve(int kase, InputReader in, Output pw) {
+			int n = in.nextInt();
+			long sum = 0;
+			int[] arr = new int[n];
+			for(int i = 0; i<n; i++) {
+				sum += arr[i] = in.nextInt();
+			}
+			{
+				long cur = 0;
+				for(int i = 0; i<n; i++) {
+					if(Utilities.even(i)) {
+						cur += arr[i]-1;
+					}
+				}
+				if(cur*2<=sum) {
+					for(int i = 0; i<n; i++) {
+						if(Utilities.even(i)) {
+							pw.print("1");
+						}else {
+							pw.print(arr[i]);
+						}
+						pw.print(" ");
+					}
+				}else {
+					for(int i = 0; i<n; i++) {
+						if(Utilities.odd(i)) {
+							pw.print("1");
+						}else {
+							pw.print(arr[i]);
+						}
+						pw.print(" ");
+					}
+				}
+			}
+			pw.println();
+		}
+
+	}
+
+	static class FastReader implements InputReader {
+		final private int BUFFER_SIZE = 1<<16;
+		private DataInputStream din;
+		private byte[] buffer;
+		private int bufferPointer;
+		private int bytesRead;
+
+		public FastReader(InputStream is) {
+			din = new DataInputStream(is);
+			buffer = new byte[BUFFER_SIZE];
+			bufferPointer = bytesRead = 0;
+		}
+
+		public int nextInt() {
+			int ret = 0;
+			byte c = skipToDigit();
+			boolean neg = (c=='-');
+			if(neg) {
+				c = read();
+			}
+			do {
+				ret = ret*10+c-'0';
+			} while((c = read())>='0'&&c<='9');
+			if(neg) {
+				return -ret;
+			}
+			return ret;
+		}
+
+		private boolean isDigit(byte b) {
+			return b>='0'&&b<='9';
+		}
+
+		private byte skipToDigit() {
+			byte ret;
+			while(!isDigit(ret = read())&&ret!='-') ;
+			return ret;
+		}
+
+		private void fillBuffer() {
+			try {
+				bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+			}catch(IOException e) {
+				e.printStackTrace();
+				throw new InputMismatchException();
+			}
+			if(bytesRead==-1) {
+				buffer[0] = -1;
+			}
+		}
+
+		private byte read() {
+			if(bytesRead==-1) {
+				throw new InputMismatchException();
+			}else if(bufferPointer==bytesRead) {
+				fillBuffer();
+			}
+			return buffer[bufferPointer++];
+		}
+
+	}
+
+	static class Output implements Closeable, Flushable {
+		public StringBuilder sb;
+		public OutputStream os;
+		public int BUFFER_SIZE;
+		public String lineSeparator;
+
+		public Output(OutputStream os) {
+			this(os, 1<<16);
+		}
+
+		public Output(OutputStream os, int bs) {
+			BUFFER_SIZE = bs;
+			sb = new StringBuilder(BUFFER_SIZE);
+			this.os = new BufferedOutputStream(os, 1<<17);
+			lineSeparator = System.lineSeparator();
+		}
+
+		public void print(int i) {
+			print(String.valueOf(i));
+		}
+
+		public void print(String s) {
+			sb.append(s);
+			if(sb.length()>BUFFER_SIZE >> 1) {
+				flushToBuffer();
+			}
+		}
+
+		public void println() {
+			sb.append(lineSeparator);
+		}
+
+		private void flushToBuffer() {
+			try {
+				os.write(sb.toString().getBytes());
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+			sb = new StringBuilder(BUFFER_SIZE);
+		}
+
+		public void flush() {
+			try {
+				flushToBuffer();
+				os.flush();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		public void close() {
+			flush();
+			try {
+				os.close();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	static interface InputReader {
+		int nextInt();
+
+	}
+
+	static class Utilities {
+		public static boolean even(int x) {
+			return (x&1)==0;
+		}
+
+		public static boolean odd(int x) {
+			return (x&1)>0;
+		}
+
+	}
+}
+
