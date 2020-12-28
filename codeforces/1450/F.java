94e20363b6a118603d1eb884e7f209ce5ed2fbbc
skittles1412 <61439840+skittles1412@users.noreply.github.com>
2020-12-28 12:18:00 -0800

    Add solution for problem `F - The Struggling Contestant` Link: https://codeforces.com/contest/1450/problem/F Tags: constructive algorithms, greedy, *2400 Ref: https://codeforces.com/contest/1450/submission/102641748

---
+++ codeforces/1450/F.java
@@ -0,0 +1,195 @@
+import java.io.*;
+import java.util.InputMismatchException;
+import java.util.function.IntUnaryOperator;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+	static class TaskAdapter implements Runnable {
+		@Override
+		public void run() {
+			long startTime = System.currentTimeMillis();
+			InputStream inputStream = System.in;
+			OutputStream outputStream = System.out;
+			FastReader in = new FastReader(inputStream);
+			Output out = new Output(outputStream);
+			FTheStrugglingContestant solver = new FTheStrugglingContestant();
+			int testCount = in.nextInt();
+			for(int i = 1; i<=testCount; i++)
+				solver.solve(i, in, out);
+			out.close();
+			System.err.println(System.currentTimeMillis()-startTime+"ms");
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Thread thread = new Thread(null, new TaskAdapter(), "", 1<<24);
+		thread.start();
+		thread.join();
+	}
+
+	static class FTheStrugglingContestant {
+		public FTheStrugglingContestant() {
+		}
+
+		public void solve(int kase, InputReader in, Output pw) {
+			int n = in.nextInt();
+			int[] arr = in.nextInt(n, o -> o-1);
+			int max = 0, mcnt = 0, x = 0;
+			int[] adj = new int[n], cnt = new int[n];
+			adj[arr[0]]++;
+			adj[arr[n-1]]++;
+			cnt[arr[n-1]]++;
+			for(int i = 0; i<n-1; i++) {
+				if(arr[i]==arr[i+1]) {
+					max = Math.max(max, adj[arr[i]] += 2);
+					x++;
+				}
+				mcnt = Math.max(mcnt, ++cnt[arr[i]]);
+			}
+			if(mcnt>(n+1)/2) {
+				pw.println("-1");
+				return;
+			}
+			pw.println(x+Math.max(0, max-(x+2)));
+		}
+
+	}
+
+	static class FastReader implements InputReader {
+		final private int BUFFER_SIZE = 1<<16;
+		private final DataInputStream din;
+		private final byte[] buffer;
+		private int bufferPointer;
+		private int bytesRead;
+
+		public FastReader(InputStream is) {
+			din = new DataInputStream(is);
+			buffer = new byte[BUFFER_SIZE];
+			bufferPointer = bytesRead = 0;
+		}
+
+		public int nextInt() {
+			int ret = 0;
+			byte c = skipToDigit();
+			boolean neg = (c=='-');
+			if(neg) {
+				c = read();
+			}
+			do {
+				ret = ret*10+c-'0';
+			} while((c = read())>='0'&&c<='9');
+			if(neg) {
+				return -ret;
+			}
+			return ret;
+		}
+
+		private boolean isDigit(byte b) {
+			return b>='0'&&b<='9';
+		}
+
+		private byte skipToDigit() {
+			byte ret;
+			while(!isDigit(ret = read())&&ret!='-') ;
+			return ret;
+		}
+
+		private void fillBuffer() {
+			try {
+				bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+			}catch(IOException e) {
+				e.printStackTrace();
+				throw new InputMismatchException();
+			}
+			if(bytesRead==-1) {
+				buffer[0] = -1;
+			}
+		}
+
+		private byte read() {
+			if(bytesRead==-1) {
+				throw new InputMismatchException();
+			}else if(bufferPointer==bytesRead) {
+				fillBuffer();
+			}
+			return buffer[bufferPointer++];
+		}
+
+	}
+
+	interface InputReader {
+		int nextInt();
+
+		default int[] nextInt(int n, IntUnaryOperator operator) {
+			int[] ret = new int[n];
+			for(int i = 0; i<n; i++) {
+				ret[i] = operator.applyAsInt(nextInt());
+			}
+			return ret;
+		}
+
+	}
+
+	static class Output implements Closeable, Flushable {
+		public StringBuilder sb;
+		public OutputStream os;
+		public int BUFFER_SIZE;
+		public String lineSeparator;
+
+		public Output(OutputStream os) {
+			this(os, 1<<16);
+		}
+
+		public Output(OutputStream os, int bs) {
+			BUFFER_SIZE = bs;
+			sb = new StringBuilder(BUFFER_SIZE);
+			this.os = new BufferedOutputStream(os, 1<<17);
+			lineSeparator = System.lineSeparator();
+		}
+
+		public void println(int i) {
+			println(String.valueOf(i));
+		}
+
+		public void println(String s) {
+			sb.append(s);
+			println();
+		}
+
+		public void println() {
+			sb.append(lineSeparator);
+		}
+
+		private void flushToBuffer() {
+			try {
+				os.write(sb.toString().getBytes());
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+			sb = new StringBuilder(BUFFER_SIZE);
+		}
+
+		public void flush() {
+			try {
+				flushToBuffer();
+				os.flush();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		public void close() {
+			flush();
+			try {
+				os.close();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+}
+
