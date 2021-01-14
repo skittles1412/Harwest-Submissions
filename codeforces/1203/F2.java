81a84231cc57a859d7d0c2e385ac9fc48e9ad4c7
skittles1412 <61439840+skittles1412@users.noreply.github.com>
2021-01-14 06:34:00 -0800

    Add solution for problem `F2 - Complete the Projects (hard version)` Link: https://codeforces.com/contest/1203/problem/F2 Tags: dp, greedy, *2300 Ref: https://codeforces.com/contest/1203/submission/104280106

---
+++ codeforces/1203/F2.java
@@ -0,0 +1,214 @@
+import java.io.BufferedOutputStream;
+import java.io.Closeable;
+import java.io.DataInputStream;
+import java.io.Flushable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.InputMismatchException;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+	static class TaskAdapter implements Runnable {
+		@Override
+		public void run() {
+			long startTime = System.currentTimeMillis();
+			InputStream inputStream = System.in;
+			OutputStream outputStream = System.out;
+			FastReader in = new FastReader(inputStream);
+			Output out = new Output(outputStream);
+			F2CompleteTheProjectsHardVersion solver = new F2CompleteTheProjectsHardVersion();
+			solver.solve(1, in, out);
+			out.close();
+			System.err.println(System.currentTimeMillis()-startTime+"ms");
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Thread thread = new Thread(null, new TaskAdapter(), "", 1<<26);
+		thread.start();
+		thread.join();
+	}
+
+	static class F2CompleteTheProjectsHardVersion {
+		public F2CompleteTheProjectsHardVersion() {
+		}
+
+		public void solve(int kase, InputReader in, Output pw) {
+			final int m = (int) 1e5;
+			int n = in.nextInt(), r = in.nextInt();
+			int[][] arr = in.nextInt(n, 2);
+			Arrays.sort(arr, Comparator.comparingInt(o -> -o[1]));
+			{
+				int j = n;
+				for(int i = 0; i<n; i++) {
+					if(arr[i][1]<0) {
+						j = Math.min(j, i);
+						arr[i][0] = Math.max(arr[i][0], -arr[i][1]);
+					}
+				}
+				Arrays.sort(arr, 0, j, Comparator.comparingInt(o -> o[0]));
+				Arrays.sort(arr, j, n, Comparator.comparingInt(o -> -o[0]-o[1]));
+			}
+			int[][] dp = new int[n+1][2*m];
+			for(int i = n-1; i>=0; i--) {
+				for(int j = 0; j<arr[i][0]; j++) {
+					dp[i][j] = dp[i+1][j];
+				}
+				for(int j = arr[i][0]; j<=m; j++) {
+					dp[i][j] = Math.max(dp[i+1][j], 1+dp[i+1][j+arr[i][1]]);
+				}
+			}
+			pw.println(dp[0][r]);
+		}
+
+	}
+
+	static interface InputReader {
+		int nextInt();
+
+		default int[] nextInt(int n) {
+			int[] ret = new int[n];
+			for(int i = 0; i<n; i++) {
+				ret[i] = nextInt();
+			}
+			return ret;
+		}
+
+		default int[][] nextInt(int n, int m) {
+			int[][] ret = new int[n][m];
+			for(int i = 0; i<n; i++) {
+				ret[i] = nextInt(m);
+			}
+			return ret;
+		}
+
+	}
+
+	static class Output implements Closeable, Flushable {
+		public StringBuilder sb;
+		public OutputStream os;
+		public int BUFFER_SIZE;
+		public String lineSeparator;
+
+		public Output(OutputStream os) {
+			this(os, 1<<16);
+		}
+
+		public Output(OutputStream os, int bs) {
+			BUFFER_SIZE = bs;
+			sb = new StringBuilder(BUFFER_SIZE);
+			this.os = new BufferedOutputStream(os, 1<<17);
+			lineSeparator = System.lineSeparator();
+		}
+
+		public void println(int i) {
+			println(String.valueOf(i));
+		}
+
+		public void println(String s) {
+			sb.append(s);
+			println();
+		}
+
+		public void println() {
+			sb.append(lineSeparator);
+		}
+
+		private void flushToBuffer() {
+			try {
+				os.write(sb.toString().getBytes());
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+			sb = new StringBuilder(BUFFER_SIZE);
+		}
+
+		public void flush() {
+			try {
+				flushToBuffer();
+				os.flush();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		public void close() {
+			flush();
+			try {
+				os.close();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	static class FastReader implements InputReader {
+		final private int BUFFER_SIZE = 1<<16;
+		private DataInputStream din;
+		private byte[] buffer;
+		private int bufferPointer;
+		private int bytesRead;
+
+		public FastReader(InputStream is) {
+			din = new DataInputStream(is);
+			buffer = new byte[BUFFER_SIZE];
+			bufferPointer = bytesRead = 0;
+		}
+
+		public int nextInt() {
+			int ret = 0;
+			byte c = skipToDigit();
+			boolean neg = (c=='-');
+			if(neg) {
+				c = read();
+			}
+			do {
+				ret = ret*10+c-'0';
+			} while((c = read())>='0'&&c<='9');
+			if(neg) {
+				return -ret;
+			}
+			return ret;
+		}
+
+		private boolean isDigit(byte b) {
+			return b>='0'&&b<='9';
+		}
+
+		private byte skipToDigit() {
+			byte ret;
+			while(!isDigit(ret = read())&&ret!='-') ;
+			return ret;
+		}
+
+		private void fillBuffer() {
+			try {
+				bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+			}catch(IOException e) {
+				e.printStackTrace();
+				throw new InputMismatchException();
+			}
+			if(bytesRead==-1) {
+				buffer[0] = -1;
+			}
+		}
+
+		private byte read() {
+			if(bytesRead==-1) {
+				throw new InputMismatchException();
+			}else if(bufferPointer==bytesRead) {
+				fillBuffer();
+			}
+			return buffer[bufferPointer++];
+		}
+
+	}
+}
+
