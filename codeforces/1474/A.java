fa8a49df22a8ba04cdbae1232c310705fb178401
skittles1412 <61439840+skittles1412@users.noreply.github.com>
2021-01-19 06:39:00 -0800

    Add solution for problem `A - Puzzle From the Future` Link: https://codeforces.com/contest/1474/problem/A Tags: greedy Ref: https://codeforces.com/contest/1474/submission/104773581

---
+++ codeforces/1474/A.java
@@ -0,0 +1,233 @@
+import java.io.*;
+import java.util.InputMismatchException;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+	static class TaskAdapter implements Runnable {
+		@Override
+		public void run() {
+			long startTime = System.currentTimeMillis();
+			InputStream inputStream = System.in;
+			OutputStream outputStream = System.out;
+			FastReader in = new FastReader(inputStream);
+			Output out = new Output(outputStream);
+			APuzzleFromTheFuture solver = new APuzzleFromTheFuture();
+			int testCount = in.nextInt();
+			for(int i = 1; i<=testCount; i++)
+				solver.solve(i, in, out);
+			out.close();
+			System.err.println(System.currentTimeMillis()-startTime+"ms");
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Thread thread = new Thread(null, new TaskAdapter(), "", 1<<26);
+		thread.start();
+		thread.join();
+	}
+
+	static class APuzzleFromTheFuture {
+		public APuzzleFromTheFuture() {
+		}
+
+		public void solve(int kase, InputReader in, Output pw) {
+			int n = in.nextInt();
+			int[] arr = in.nextIntChar(o -> o-'0');
+			StringBuilder ans = new StringBuilder(n);
+			int prev = 0;
+			for(int i = 0; i<n; i++) {
+				if(prev==2) {
+					ans.append(arr[i]^1);
+					prev = 1;
+				}else if(prev==1) {
+					if(arr[i]==0) {
+						ans.append(0);
+						prev = 0;
+					}else {
+						ans.append(1);
+						prev = 2;
+					}
+				}else if(prev==0) {
+					ans.append(1);
+					prev = arr[i]+1;
+				}
+			}
+			pw.println(ans);
+		}
+
+	}
+
+	static class Output implements Closeable, Flushable {
+		public StringBuilder sb;
+		public OutputStream os;
+		public int BUFFER_SIZE;
+		public String lineSeparator;
+
+		public Output(OutputStream os) {
+			this(os, 1<<16);
+		}
+
+		public Output(OutputStream os, int bs) {
+			BUFFER_SIZE = bs;
+			sb = new StringBuilder(BUFFER_SIZE);
+			this.os = new BufferedOutputStream(os, 1<<17);
+			lineSeparator = System.lineSeparator();
+		}
+
+		public void print(String s) {
+			sb.append(s);
+			if(sb.length()>BUFFER_SIZE >> 1) {
+				flushToBuffer();
+			}
+		}
+
+		public void println(Object... o) {
+			for(int i = 0; i<o.length; i++) {
+				if(i!=0) {
+					print(" ");
+				}
+				print(String.valueOf(o[i]));
+			}
+			println();
+		}
+
+		public void println() {
+			sb.append(lineSeparator);
+		}
+
+		private void flushToBuffer() {
+			try {
+				os.write(sb.toString().getBytes());
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+			sb = new StringBuilder(BUFFER_SIZE);
+		}
+
+		public void flush() {
+			try {
+				flushToBuffer();
+				os.flush();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		public void close() {
+			flush();
+			try {
+				os.close();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	static class FastReader implements InputReader {
+		final private int BUFFER_SIZE = 1<<16;
+		private final DataInputStream din;
+		private final byte[] buffer;
+		private int bufferPointer;
+		private int bytesRead;
+
+		public FastReader(InputStream is) {
+			din = new DataInputStream(is);
+			buffer = new byte[BUFFER_SIZE];
+			bufferPointer = bytesRead = 0;
+		}
+
+		public String next() {
+			StringBuilder ret = new StringBuilder(64);
+			byte c = skip();
+			while(c!=-1&&!isSpaceChar(c)) {
+				ret.appendCodePoint(c);
+				c = read();
+			}
+			return ret.toString();
+		}
+
+		public int nextInt() {
+			int ret = 0;
+			byte c = skipToDigit();
+			boolean neg = (c=='-');
+			if(neg) {
+				c = read();
+			}
+			do {
+				ret = ret*10+c-'0';
+			} while((c = read())>='0'&&c<='9');
+			if(neg) {
+				return -ret;
+			}
+			return ret;
+		}
+
+		private boolean isSpaceChar(byte b) {
+			return b==' '||b=='\r'||b=='\n'||b=='\t'||b=='\f';
+		}
+
+		private byte skip() {
+			byte ret;
+			while(isSpaceChar((ret = read()))) ;
+			return ret;
+		}
+
+		private boolean isDigit(byte b) {
+			return b>='0'&&b<='9';
+		}
+
+		private byte skipToDigit() {
+			byte ret;
+			while(!isDigit(ret = read())&&ret!='-') ;
+			return ret;
+		}
+
+		private void fillBuffer() {
+			try {
+				bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+			}catch(IOException e) {
+				e.printStackTrace();
+				throw new InputMismatchException();
+			}
+			if(bytesRead==-1) {
+				buffer[0] = -1;
+			}
+		}
+
+		private byte read() {
+			if(bytesRead==-1) {
+				throw new InputMismatchException();
+			}else if(bufferPointer==bytesRead) {
+				fillBuffer();
+			}
+			return buffer[bufferPointer++];
+		}
+
+	}
+
+	interface InputReader {
+		String next();
+
+		int nextInt();
+
+		default int[] nextIntChar(InputReader.CharToIntFunction f) {
+			String s = next();
+			int[] ret = new int[s.length()];
+			for(int i = 0; i<s.length(); i++) {
+				ret[i] = f.apply(s.charAt(i));
+			}
+			return ret;
+		}
+
+		interface CharToIntFunction {
+			int apply(char c);
+
+		}
+
+	}
+}
+
