03812a2ff53ed96eb8733f66e78bf1aa9e953222
skittles1412 <61439840+skittles1412@users.noreply.github.com>
2021-01-30 11:53:00 -0800

    Add solution for problem `C - Ciel the Commander` Link: https://codeforces.com/contest/321/problem/C Tags: constructive algorithms, dfs and similar, divide and conquer, greedy, trees, *2100 Ref: https://codeforces.com/contest/321/submission/106053516

---
+++ codeforces/321/C.java
@@ -0,0 +1,254 @@
+import java.io.BufferedOutputStream;
+import java.io.Closeable;
+import java.io.DataInputStream;
+import java.io.Flushable;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+
+/**
+ * Built using CHelper plug-in
+ * Actual solution is at the top
+ */
+public class Main {
+	static class TaskAdapter implements Runnable {
+		@Override
+		public void run() {
+			long startTime = System.currentTimeMillis();
+			InputStream inputStream = System.in;
+			OutputStream outputStream = System.out;
+			FastReader in = new FastReader(inputStream);
+			Output out = new Output(outputStream);
+			CCielTheCommander solver = new CCielTheCommander();
+			solver.solve(1, in, out);
+			out.close();
+			System.err.println(System.currentTimeMillis()-startTime+"ms");
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Thread thread = new Thread(null, new TaskAdapter(), "", 1<<26);
+		thread.start();
+		thread.join();
+	}
+
+	static class CCielTheCommander {
+		int[] size;
+		int[] rank;
+		boolean[] marked;
+		ArrayList<Integer>[] graph;
+
+		public CCielTheCommander() {
+		}
+
+		public void dfs(int u, int p) {
+			size[u] = 1;
+			for(int v: graph[u]) {
+				if(v!=p) {
+					dfs(v, u);
+					size[u] += size[v];
+				}
+			}
+		}
+
+		public int decomp(int u, int r) {
+			for(int v: graph[u]) {
+				if(!marked[v]&&size[v]>size[u]/2) {
+					size[u] -= size[v];
+					size[v] += size[u];
+					return decomp(v, r);
+				}
+			}
+			rank[u] = r;
+			marked[u] = true;
+			for(int v: graph[u]) {
+				if(!marked[v]) {
+					decomp(v, r+1);
+				}
+			}
+			return u;
+		}
+
+		public void solve(int kase, InputReader in, Output pw) {
+			long start = System.currentTimeMillis();
+			int n = in.nextInt();
+			graph = in.nextUndirectedGraph(n, n-1);
+			size = new int[n];
+			dfs(0, -1);
+			rank = new int[n];
+			marked = new boolean[n];
+			decomp(0, 0);
+			if(System.currentTimeMillis()-start>=600) {
+				System.out.println(System.currentTimeMillis()-start);
+				return;
+			}
+			char[] ans = new char[2*n];
+			Arrays.fill(ans, ' ');
+			for(int i = 0; i<n; i++) {
+				ans[i*2] = (char) (rank[i]+'A');
+			}
+			System.out.println(ans);
+		}
+
+	}
+
+	static class FastReader implements InputReader {
+		final private int BUFFER_SIZE = 1<<16;
+		private DataInputStream din;
+		private byte[] buffer;
+		private int bufferPointer;
+		private int bytesRead;
+
+		public FastReader(InputStream is) {
+			din = new DataInputStream(is);
+			buffer = new byte[BUFFER_SIZE];
+			bufferPointer = bytesRead = 0;
+		}
+
+		public int nextInt() {
+			int ret = 0;
+			byte c = skipToDigit();
+			boolean neg = (c=='-');
+			if(neg) {
+				c = read();
+			}
+			do {
+				ret = ret*10+c-'0';
+			} while((c = read())>='0'&&c<='9');
+			if(neg) {
+				return -ret;
+			}
+			return ret;
+		}
+
+		private boolean isDigit(byte b) {
+			return b>='0'&&b<='9';
+		}
+
+		private byte skipToDigit() {
+			byte ret;
+			while(!isDigit(ret = read())&&ret!='-') ;
+			return ret;
+		}
+
+		private void fillBuffer() {
+			try {
+				bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+			}catch(IOException e) {
+				e.printStackTrace();
+				throw new InputMismatchException();
+			}
+			if(bytesRead==-1) {
+				buffer[0] = -1;
+			}
+		}
+
+		private byte read() {
+			if(bytesRead==-1) {
+				throw new InputMismatchException();
+			}else if(bufferPointer==bytesRead) {
+				fillBuffer();
+			}
+			return buffer[bufferPointer++];
+		}
+
+	}
+
+	static class Output implements Closeable, Flushable {
+		public StringBuilder sb;
+		public OutputStream os;
+		public int BUFFER_SIZE;
+		public String lineSeparator;
+
+		public Output(OutputStream os) {
+			this(os, 1<<16);
+		}
+
+		public Output(OutputStream os, int bs) {
+			BUFFER_SIZE = bs;
+			sb = new StringBuilder(BUFFER_SIZE);
+			this.os = new BufferedOutputStream(os, 1<<17);
+			lineSeparator = System.lineSeparator();
+		}
+
+		public void print(String s) {
+			sb.append(s);
+			if(sb.length()>BUFFER_SIZE >> 1) {
+				flushToBuffer();
+			}
+		}
+
+		public void println(Object... o) {
+			for(int i = 0; i<o.length; i++) {
+				if(i!=0) {
+					print(" ");
+				}
+				print(String.valueOf(o[i]));
+			}
+			println();
+		}
+
+		public void println() {
+			sb.append(lineSeparator);
+		}
+
+		private void flushToBuffer() {
+			try {
+				os.write(sb.toString().getBytes());
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+			sb = new StringBuilder(BUFFER_SIZE);
+		}
+
+		public void flush() {
+			try {
+				flushToBuffer();
+				os.flush();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		public void close() {
+			flush();
+			try {
+				os.close();
+			}catch(IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	static interface InputReader {
+		int nextInt();
+
+		default ArrayList<Integer>[] nextUndirectedGraph(int n, int m) {
+			ArrayList<Integer>[] ret = new ArrayList[n];
+			for(int i = 0; i<n; i++) {
+				ret[i] = new ArrayList<>();
+			}
+			for(int i = 0; i<m; i++) {
+				int u = nextInt()-1, v = nextInt()-1;
+				ret[u].add(v);
+				ret[v].add(u);
+			}
+			return ret;
+		}
+
+	}
+}
+
+
+
+
+
+
+
+
+
