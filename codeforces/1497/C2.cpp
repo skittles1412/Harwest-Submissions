c1a06f7ce55f2dc64cd64684856c807993873572
skittles1412 <61439840+skittles1412@users.noreply.github.com>
2021-03-17 13:57:00 -0700

    Add solution for problem `C2 - k-LCM (hard version)` Link: https://codeforces.com/contest/1497/problem/C2 Tags: constructive algorithms, math, *1600 Ref: https://codeforces.com/contest/1497/submission/110261545

---
+++ codeforces/1497/C2.cpp
@@ -0,0 +1,63 @@
+#include "bits/stdc++.h"
+
+using namespace std;
+
+//sad; long long double doesn't exist
+using ld = long double;
+
+//imagine a language where int = long
+#define long long long
+
+//typing too hard
+#define endl "\n"
+
+#define sz(x) int((x).size())
+
+vector<int> solve(int x) {
+	assert(x >= 3);
+	if(x & 1) {
+		return {x / 2, x / 2, 1};
+	}else if(x == 4) {
+		return {1, 1, 2};
+	}else if(x == 6) {
+		return {2, 2, 2};
+	}else {
+		auto ans = solve(x / 2);
+		for(auto &a: ans) {
+			a *= 2;
+		}
+		return ans;
+	}
+}
+
+void solve() {
+	int n, k;
+	cin >> n >> k;
+	vector<int> ans;
+	while(k > 3) {
+		ans.push_back(1);
+		k--;
+		n--;
+	}
+	auto e = solve(n);
+	for(auto &a: ans) {
+		cout << a << " ";
+	}
+	for(auto &a: e) {
+		cout << a << " ";
+	}
+	cout << endl;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    cin.exceptions(ios::failbit);
+#ifdef LOCAL
+	freopen("input.txt", "r", stdin);
+#endif
+    int t;
+    cin >> t;
+    while(t--) {
+    	solve();
+    }
+}
