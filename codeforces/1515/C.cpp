5913d729fc98e6973ed42def1f5364be3b73ffed
skittles1412 <61439840+skittles1412@users.noreply.github.com>
2021-05-02 15:40:00 -0700

    Add solution for problem `C - Phoenix and Towers` Link: https://codeforces.com/contest/1515/problem/C Tags: constructive algorithms, data structures, greedy, *1400 Ref: https://codeforces.com/contest/1515/submission/114973212

---
+++ codeforces/1515/C.cpp
@@ -0,0 +1,71 @@
+#include "bits/extc++.h"
+
+using namespace std;
+
+template<class T, class U = less<T>>
+using rt = __gnu_pbds::tree<T, __gnu_pbds::null_type, U, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update>;
+
+template<class T, class U>
+void sep(T &out, const string &s, const U &u) {
+	out << u;
+}
+
+template<class T, class Head, class ...Tail>
+void sep(T &out, const string &s, const Head &h, const Tail &...t) {
+	out << h << s;
+	sep(out, s, t...);
+}
+
+#ifdef DEBUG
+#define dbg(...)                                                      \
+cerr << "L" << __LINE__ << " [" << #__VA_ARGS__ << "]" << ": ";       \
+sep(cerr, " | ", __VA_ARGS__);                                        \
+cerr << endl
+#else
+#define cerr if(false) cerr
+#define dbg(...) cerr
+#endif
+
+//imagine a language where int = long
+#define long int64_t
+
+//typing too hard
+#define endl "\n"
+
+#define sz(x) int((x).size())
+
+void solve() {
+	int n, m, x;
+	cin >> n >> m >> x;
+	set<pair<int, int>> s;
+	for(int i = 0; i < m; i++) {
+		s.emplace(0, i);
+	}
+	cout << "YES" << endl;
+	for(int i = 0; i < n; i++) {
+		int h;
+		cin >> h;
+		auto it = s.begin();
+		auto c = *it;
+		s.erase(it);
+		c.first += h;
+		cout << c.second + 1 << " ";
+		s.insert(c);
+	}
+	cout << endl;
+}
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+	cin.exceptions(ios::failbit);
+#ifdef LOCAL
+	freopen("input.txt", "r", stdin);
+#endif
+	int t = 1;
+	cin >> t;
+	for(int _ = 1; _ <= t; _++) {
+		dbg(_);
+//		cout << "Case #" << _ << ": ";
+		solve();
+	}
+}
