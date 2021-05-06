d8573ab318bd8777ce538bc204e5f31ab099c8c8
skittles1412 <61439840+skittles1412@users.noreply.github.com>
2021-05-06 09:33:00 -0700

    Add solution for problem `G - To Go Or Not To Go?` Link: https://codeforces.com/contest/1520/problem/G Tags: brute force, dfs and similar, graphs, greedy, shortest paths, *2200 Ref: https://codeforces.com/contest/1520/submission/115439801

---
+++ codeforces/1520/G.cpp
@@ -0,0 +1,167 @@
+#pragma GCC optimize("unroll-loops")
+
+#include "bits/extc++.h"
+
+using namespace std;
+
+template<class T, class U = less<T>>
+using rt = __gnu_pbds::tree<T, __gnu_pbds::null_type, U, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update>;
+
+template<class T, class U>
+void sep(T &out, const string &s, const U &u) {
+	out << u;
+}
+
+template<class T, class Head, class ...Tail>
+void sep(T &out, const string &s, const Head &h, const Tail &...t) {
+	out << h << s;
+	sep(out, s, t...);
+}
+
+#ifdef DEBUG
+#define dbg(...)                                                      \
+cerr << "L" << __LINE__ << " [" << #__VA_ARGS__ << "]" << ": ";       \
+sep(cerr, " | ", __VA_ARGS__);                                        \
+cerr << endl
+#else
+#define cerr if(false) cerr
+#define dbg(...) cerr
+#endif
+
+//imagine a language where int = long
+#define long int64_t
+
+//typing too hard
+#define endl "\n"
+
+#define sz(x) int((x).size())
+
+// https://github.com/bqi343/USACO/blob/master/Implementations/content/various/FastIO.h
+namespace FastIO {
+	const int BSZ = 1 << 15; ////// INPUT
+	char ibuf[BSZ];
+	int ipos, ilen;
+
+	inline char nc() { // next char
+		if(ipos == ilen) {
+			ipos = 0;
+			ilen = fread(ibuf, 1, BSZ, stdin);
+			if(!ilen)
+				return EOF;
+		}
+		return ibuf[ipos++];
+	}
+
+	template<class T>
+	inline void ri(T &x) { // read int or ll
+		char ch;
+		int sgn = 1;
+		while(!isdigit(ch = nc()))
+			if(ch == '-')
+				sgn *= -1;
+		x = ch - '0';
+		while(isdigit(ch = nc()))
+			x = x * 10 + (ch - '0');
+		x *= sgn;
+	}
+}
+using namespace FastIO;
+
+const int maxn = 2e3 + 2, inf = 0x7f7f7f7f;
+const long linf = 1e18;
+const int dx[4] {0, 1, 0, -1}, dy[4] {1, 0, -1, 0};
+
+int n, m;
+long w;
+int qx[maxn * maxn], qy[maxn * maxn], dist1[maxn][maxn], dist2[maxn][maxn], arr[maxn][maxn];
+
+void bfs1() {
+	memset(dist1, 0x7f, sizeof(dist1));
+	qx[0] = qy[0] = 1;
+	int l = 0, r = 1;
+	dist1[1][1] = 0;
+	while(l < r) {
+		int x = qx[l], y = qy[l++], d = dist1[x][y] + 1;
+		for(int i = 0; i < 4; i++) {
+			int cx = x + dx[i];
+			int cy = y + dy[i];
+			if(arr[cx][cy] != -1 && dist1[cx][cy] == inf) {
+				dist1[cx][cy] = d;
+				qx[r] = cx;
+				qy[r++] = cy;
+			}
+		}
+	}
+}
+
+void bfs2() {
+	memset(dist2, 0x7f, sizeof(dist2));
+	qx[0] = n;
+	qy[0] = m;
+	int l = 0, r = 1;
+	dist2[n][m] = 0;
+	while(l < r) {
+		int x = qx[l], y = qy[l++], d = dist2[x][y] + 1;
+		for(int i = 0; i < 4; i++) {
+			int cx = x + dx[i];
+			int cy = y + dy[i];
+			if(arr[cx][cy] != -1 && dist2[cx][cy] == inf) {
+				dist2[cx][cy] = d;
+				qx[r] = cx;
+				qy[r++] = cy;
+			}
+		}
+	}
+}
+
+void solve() {
+	memset(arr, -1, sizeof(arr));
+	ri(n);
+	ri(m);
+	ri(w);
+	for(int i = 1; i <= n; i++) {
+		for(int j = 1; j <= m; j++) {
+			ri(arr[i][j]);
+		}
+	}
+	bfs1();
+	bfs2();
+	long ports = linf, porte = linf;
+	for(int i = 0; i < maxn; i++) {
+		for(int j = 0; j < maxn; j++) {
+			if(arr[i][j] > 0) {
+				if(dist1[i][j] != inf) {
+					ports = min(ports, arr[i][j] + w * dist1[i][j]);
+				}
+				if(dist2[i][j] != inf) {
+					porte = min(porte, arr[i][j] + w * dist2[i][j]);
+				}
+			}
+		}
+	}
+	if(max(ports, porte) == linf && dist2[1][1] == inf) {
+		cout << -1 << endl;
+	}else {
+		long ans = linf;
+		ans = min(ans, w * dist2[1][1]);
+		dbg(ans);
+		ans = min(ans, ports + porte);
+		dbg(ans);
+		cout << ans << endl;
+	}
+}
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+	cin.exceptions(ios::failbit);
+#ifdef LOCAL
+	freopen("input.txt", "r", stdin);
+#endif
+	int t = 1;
+//	cin >> t;
+	for(int _ = 1; _ <= t; _++) {
+		dbg(_);
+//		cout << "Case #" << _ << ": ";
+		solve();
+	}
+}
